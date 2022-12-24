971111929596aa3b2f94981ca8e9b8dfb035c736
NaveOWO <pianowell96@gmail.com>
2022-12-25 03:45:11 +0900

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./style/globalStyle";
+import Router from "./Router";
+import { theme } from "./style/theme";
+import { RecoilRoot } from "recoil";
+import React from "react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <React.StrictMode>
+        <RecoilRoot>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Router />
+          </ThemeProvider>
+        </RecoilRoot>
+      </React.StrictMode>
+    </>
+  );
 }
 
 export default App;
