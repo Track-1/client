057634a6c71ecb8a7a0408d390ee7ea1791804d5
myeongheonhong <ruddyh7076@gmail.com>
2022-12-27 22:28:05 +0900

    [ design ] : header UI 1차 구현

--- src/@components/@common/header.tsx
+++ src/@components/@common/header.tsx
@@ -1,3 +1,48 @@
+import styled from "styled-components";
+import logo from "../../assets/image/logo.svg";
+import slogan from "../../assets/image/slogan.svg";
+import userImg from "../../assets/image/userImg.svg";
+
 export default function Header() {
-  return <>header</>;
+  return (
+    <>
+      <HeaderContainer>
+        <img src={logo} alt="로고" />
+        <img src={slogan} alt="슬로건" />
+        <MyPageBtn type="button">
+          <UserImg src={userImg} />
+          My Page
+        </MyPageBtn>
+      </HeaderContainer>
+    </>
+  );
 }
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  height: 143px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 69px 0 75px;
+`;
+
+const MyPageBtn = styled.button`
+  width: 195px;
+  height: 52px;
+
+  display: flex;
+  align-items: center;
+  margin: 20px;
+  border-radius: 30px;
+  background-color: ${({ theme }) => theme.colors.main};
+`;
+
+const UserImg = styled.img`
+  width: 31px;
+  height: 31px;
+
+  border-radius: 50px;
+  border: 1px solid white;
+`;
--- src/@pages/mainPage.tsx
+++ src/@pages/mainPage.tsx
@@ -1,3 +1,17 @@
+import Header from "../@components/@common/header";
+import styled from "styled-components";
 export default function MainPage() {
-  return <>main-page</>;
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.sub3};
+`;
