a6798cbccd1decdfc0893f9f621e5e1765b2c09b
Seojisoo20191941 <amy0480@naver.com>
2022-12-28 16:41:04 +0900

    [ design ] 헤더 디자인 완료

--- src/@components/@common/categoryHeader.tsx
+++ src/@components/@common/categoryHeader.tsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import { ToggleIcon, Track1Icon } from '../../assets/icon'
+import styled from 'styled-components'
+
+import { ToggleIcon, Track1Icon, BtnTracks, BtnVocals } from '../../assets/icon'
 import profileImgSrc from '../../assets/image/profileImg.png'
 
 export default function categoryHeader() {
   return (
-    <header>
-        <Track1Icon/>
-        <p>Tracks</p>
-        <p>Vocals</p>
-        <img src={profileImgSrc} alt="프로필이미지"/>
-        <ToggleIcon/>
-    </header>
+    <>
+    <CategoryContainer>
+        <CategoryWrapper>
+            <BtnTracks/>
+            <BtnVocals/>
+        </CategoryWrapper>
+    </CategoryContainer>
+
+    <HeaderWrapper>
+        <TrackOneIcon/>
+        <ProfileWrapper>
+            <img src={profileImgSrc} alt="프로필이미지"/>
+            <ToggleIcon/>
+        </ProfileWrapper>
+    </HeaderWrapper>
+    </>
   )
 }
+
+const HeaderWrapper=styled.header`
+    display: flex;
+    justify-content: space-between;
+
+    width: 192rem;
+    height: 14.3rem;
+
+    background-color: black;
+    color: white;
+`
+
+const TrackOneIcon=styled(Track1Icon)`
+    margin: 4.1rem 0 0 5.8rem;
+`
+
+const CategoryContainer=styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const CategoryWrapper=styled.div`
+    margin-top:3.73rem;
+    
+    position: absolute;
+    z-index: 2;
+`
+
+const ProfileWrapper=styled.div`
+    display: flex;
+    align-items: center;
+    
+    margin-right: 4.9rem;
+
+    & > img{
+        margin-right: 1.29rem;
+    }
+`
--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,4 @@
 export { ReactComponent as Track1Icon } from './trackOneIcon.svg';
 export { ReactComponent as ToggleIcon } from './toggleIcon.svg';
+export { ReactComponent as BtnTracks } from './btn_Tracks.svg';
+export { ReactComponent as BtnVocals } from './btn_Vocals.svg';
