611e958ee13e1770ebdaf2cfcb6f76bf1281b1c2
Seojisoo20191941 <amy0480@naver.com>
2022-12-29 02:17:23 +0900

    [ feat ] 기본 카테고리 형식 구현

--- src/@components/trackSearch/categoryList.tsx
+++ src/@components/trackSearch/categoryList.tsx
@@ -1,11 +1,25 @@
 import categorys from '../../mocks/categoryDummy.json'
+import styled from 'styled-components'
 
 export default function CategoryList() {
   return (
-    <>
+    <CategoryListWrapper>
     {categorys.map(({id, category})=>(
-      <p key={id}>{category}</p>
+      <CategoryText key={id}>{category}</CategoryText>
     ))}
-    </>
+    </CategoryListWrapper>
   )
 }
+
+const CategoryListWrapper=styled.section`
+  display: flex;
+  flex-direction: column;
+
+  margin:4.652rem 0 0 7.596rem;
+`
+
+const CategoryText=styled.p`
+  margin-bottom: 5.4rem;
+  ${({ theme }) => theme.fonts.title};
+  color:${({ theme }) => theme.colors.white};
+`
