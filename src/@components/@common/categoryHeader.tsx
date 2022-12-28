1fc4d36afbcd8bfbf3186dbe29f4a0d6beac0b42
Seojisoo20191941 <amy0480@naver.com>
2022-12-28 16:01:01 +0900

    [ feat ] 헤더 구현

--- src/@components/@common/categoryHeader.tsx
+++ src/@components/@common/categoryHeader.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { ToggleIcon, Track1Icon } from '../../assets/icon'
+import profileImgSrc from '../../assets/image/profileImg.png'
 
 export default function categoryHeader() {
   return (
-    <div>categoryHeader</div>
+    <header>
+        <Track1Icon/>
+        <p>Tracks</p>
+        <p>Vocals</p>
+        <img src={profileImgSrc} alt="프로필이미지"/>
+        <ToggleIcon/>
+    </header>
   )
 }
