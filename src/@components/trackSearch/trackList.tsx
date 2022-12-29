b04aab7f66ea2e61bc45e21df88b2f7126d615bf
Seojisoo20191941 <amy0480@naver.com>
2022-12-29 21:43:08 +0900

    [ desgin ] 트랙 리스트 스타일링

--- src/@components/trackSearch/trackList.tsx
+++ src/@components/trackSearch/trackList.tsx
@@ -12,17 +12,19 @@ export default function TrackList() {
         <Hashtag/>
     </CategoryWrapper>
 
+    <TracksWrapper>
     {tracks.map(({id, imgSrc, title, producer, category, hashtags})=>(
-        <>
+        <Tracks>
         <div key={id}>
             <img src={require('../../assets/image/'+ imgSrc + '.png')} alt="썸네일"/>
-            {title}
-            {producer}
-            {category}
+            <div>{title}</div>
+            <div>{producer}</div>
+            <div>{category}</div>
         </div>
         {hashtags.map((tag, idx)=>(<p key={idx}>{tag}</p>))}
-        </>
+        </Tracks>
     ))}
+    </TracksWrapper>
     </>
   )
 }
@@ -43,4 +45,56 @@ const Category=styled(CategoryTextIc)`
 
 const Hashtag=styled(HashtagTextIc)`
     margin-left:10.7rem;
+`
+
+const TracksWrapper=styled.section`
+    ${({ theme }) => theme.fonts.body1}
+    color: ${({ theme }) => theme.colors.white};
+`
+
+const Tracks=styled.article`
+    display: flex;
+    align-items: center;
+
+    width: 154.3rem;
+    height: 12.1rem;
+
+    margin-left: 6.6rem;
+    margin-bottom: 0.7rem;
+
+    border:0.15rem solid transparent;
+    background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}), 
+    linear-gradient(to right, ${({ theme }) => theme.colors.sub1} 0%, ${({ theme }) => theme.colors.sub3} 90%,  ${({ theme }) => theme.colors.sub3} 100%);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+    border-radius: 117px 0px 0px 117px;
+
+    & > div{
+        display: flex;
+        align-items: center;
+
+        margin-left: 2.4rem;
+
+        & > img{
+            width: 8.3rem;
+            height: 8.3rem;
+
+            border-radius: 6.55rem;
+        }
+
+        & > div:nth-child(2){
+            width: 35rem;
+            padding-left: 2.8rem;
+        }
+
+        & > div:nth-child(3){
+            width: 21.3rem;
+        }
+
+        & > div:nth-child(4){
+            width: 21rem;
+        }
+
+
+    }
 `
