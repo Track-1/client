a6865f7f7d7a01d473542e765352c3611acf578e
NaveOWO <pianowell96@gmail.com>
2023-01-01 02:24:50 +0900

    [ design ] 해시태그 컴포넌트 구현

---
+++ src/@components/trackPost/hashTag.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface HashTag {
+  text: string;
+}
+
+export default function HashTag(props: HashTag) {
+  const { text } = props;
+  return <TagBox>{text}</TagBox>;
+}
+
+const TagBox = styled.article`
+  height: 3.8rem;
+  width: 17.3rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 2.1rem;
+
+  margin-bottom: 1.2rem;
+  margin-right: 0.8rem;
+
+  background-color: ${({ theme }) => theme.colors.gray4};
+
+  color: ${({ theme }) => theme.colors.white};
+
+  ${({ theme }) => theme.fonts.hashtag}
+`;
