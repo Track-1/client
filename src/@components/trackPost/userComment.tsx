d357f850b26a29e09dd0e0fe98d063e39d1a69e9
NaveOWO <pianowell96@gmail.com>
2023-01-02 03:45:20 +0900

    [ feat ] 유저 댓글 전체 컴포넌트 구현

---
+++ src/@components/trackPost/userComment.tsx
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import { AddCommentIc, CommentBtnIc } from "../../assets";
+import CommentWrite from "./commentWrite";
+import EachUseComment from "./eachUserComment";
+import comments from "../../core/trackPost/userComments";
+import { useState } from "react";
+import { UploadDataType } from "../../type/uploadDataType";
+
+export default function UseComment() {
+  const [uploadData, setUploadData] = useState<UploadDataType>({
+    text: "",
+    file: null,
+  });
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
+  function uploadComment() {
+    setIsCompleted(true);
+  }
+
+  function getUploadData(text: string, audioFile: File | null) {
+    setUploadData({
+      text: text,
+      file: audioFile,
+    });
+  }
+
+  return (
+    <CommentContainer>
+      <CloseCommentBtn>
+        <CommentBtnIcon />
+        <CloseText>Close</CloseText>
+      </CloseCommentBtn>
+      <CommentWrite getUploadData={getUploadData} isCompleted={isCompleted} />
+      <AddWrapper>
+        <div></div>
+        <AddCommentIcon onClick={uploadComment} />
+      </AddWrapper>
+      {comments.map((data, index) => {
+        // data.isMe ? : merge할 때 분기처리
+        return <EachUseComment key={index} data={data} />;
+      })}
+      <BlurSection />
+    </CommentContainer>
+  );
+}
+
+const CommentContainer = styled.section`
+  width: 107.7rem;
+  float: right;
+
+  background-color: rgba(13, 14, 17, 0.75);
+
+  padding-left: 2.6rem;
+  padding-top: 6.1rem;
+`;
+
+const CloseCommentBtn = styled.div`
+  width: 19.8rem;
+
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 2.7rem;
+`;
+
+const CloseText = styled.strong`
+  ${({ theme }) => theme.fonts.id};
+
+  color: ${({ theme }) => theme.colors.white};
+
+  margin-left: 0.5rem;
+`;
+
+const CommentBtnIcon = styled(CommentBtnIc)`
+  height: 2rem;
+  width: 100%;
+`;
+
+const AddWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+const AddCommentIcon = styled(AddCommentIc)`
+  margin-top: 1.9rem;
+  margin-bottom: 1.4rem;
+`;
+
+const BlurSection = styled.div`
+  height: 32rem;
+  width: 101.1rem;
+
+  background: linear-gradient(360deg, #000000 27.81%, rgba(0, 0, 0, 0) 85.65%);
+
+  bottom: 0;
+  right: 0;
+  position: sticky;
+`;
