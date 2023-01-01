5e0877327d57a26a42d14b12791f08d4d25e7c99
Seojisoo20191941 <amy0480@naver.com>
2023-01-02 01:31:34 +0900

    [ feat ] 카테고리 api 리코일 기본값 변경

--- src/recoil/categorySelect.ts
+++ src/recoil/categorySelect.ts
@@ -2,5 +2,5 @@ import { atom } from 'recoil';
 
 export const categorySelect = atom({
   key: 'categorySelect',
-  default: ``,
+  default: '&categ=0&categ=1&categ=2&categ=3&categ=4&categ=5&categ=6&categ=7&categ=8',
 });
