2cb445ca97894bad473e452f064b899029551ab7
myeongheonhong <ruddyh7076@gmail.com>
2023-01-02 14:23:26 +0900

    [ fix ] : 배너이미지 리셋되면서 버벅임 현상 해결

--- src/@components/@common/footer.tsx
+++ src/@components/@common/footer.tsx
@@ -1,5 +1,5 @@
 import styled, { keyframes } from "styled-components";
-import bannerImg from "../../assets/image/bannerImg.svg";
+import bannerImg from "../../assets/image/bannerImg.png";
 
 export default function Footer() {
   return (
@@ -19,17 +19,16 @@ const FooterContainer = styled.footer`
 `;
 
 const LinearFlow = keyframes`
-  from {background-position : 0rem;}
-  to { background-position : -192rem;}
+  0% {background-position : 0rem;}
+  100% { background-position : -192rem};
+
 `;
 
 const Banner = styled.div`
   height: 6.2rem;
 
   margin-top: 3.1rem;
-  background-image: url(${bannerImg});
-  background-repeat: no-repeat;
-  background-repeat: repeat-x;
+  background: url(${bannerImg}) 0 center/192rem repeat-x;
   -webkit-animation: ${LinearFlow} 15s infinite linear;
   -moz-animation: ${LinearFlow} 15s infinite linear;
   -o-animation: ${LinearFlow} 15s infinite linear;
@@ -40,7 +39,7 @@ const Copyright = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 4.2rem;
-  color: ${({ theme }) => theme.colors.white};
+  /*  */
 `;
 
 const CopyrightText = styled.p`
