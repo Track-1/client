2bb723685bda1b7fe777119ec8f895c60f901278
Seojisoo20191941 <amy0480@naver.com>
2023-01-03 02:43:57 +0900

    [ feat ] 플레이어 컴포넌트, 리코일 추가

--- src/@pages/vocalProfilePage.tsx
+++ src/@pages/vocalProfilePage.tsx
@@ -1,9 +1,21 @@
+import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
+import Player from "../@components/@common/player";
 import VocalProfileShadow from "../@components/vocalProfile/vocalProfileShadow";
+import {showPlayerBar} from "../recoil/player"
+import { tracksOrVocalsCheck } from "../recoil/tracksOrVocalsCheck";
+
 
 export default function VocalProfilePage() {
+  const showPlayer=useRecoilValue<boolean>(showPlayerBar);
+  const [whom, setWhom]=useRecoilState(tracksOrVocalsCheck);
+
+  setWhom("Vocals") 
+
   return (
     <>
+    {showPlayer&&(<Player />)}
+
     <VocalProfileWrapper>
       <VocalProfileShadow/>
     </VocalProfileWrapper>
