46bf30e79dcefa7c65fbdbad3620ae745c6757b8
[talkingOrange] <sonrisa-bonita@naver.com>
2023-01-05 12:58:25 +0900

    [ etc ] 코드 위치 수정

--- src/@components/vocalSearch/vocalList.tsx
+++ src/@components/vocalSearch/vocalList.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { VocalSleepIc, VocalNonSleepIc, VocalHoverPlayIc, VocalHoverPauseIc } from "../../assets";
 import vocals from "../../mocks/vocalsListDummy.json";
 import { showPlayerBar, playMusic, trackClicked, selectedId } from "../../recoil/player";
-import { useRecoilState } from "recoil";
-import { useNavigate } from "react-router-dom";
 
 export default function VocalList() {
   const [hoverVocal, setHoverVocal] = useState<number>(-1);
@@ -55,7 +55,7 @@ export default function VocalList() {
             <CategoryNum>+{categoryNum}</CategoryNum>
           </CategoryTextWrapper>
 
-          <MusicProfile
+          <MusicProfileWrapper
             onMouseLeave={mouseOutVocal}
             onMouseEnter={() => mouseOverVocal(id)}
             onClick={() => {
@@ -66,13 +66,13 @@ export default function VocalList() {
             hoverVocalBool={hoverVocal === id}
             clickVocalBool={clickVocal === id}
             clickVocal={clickVocal}>
-            <GradientEffect>
+            <GradientLine>
               <AlbumCoverImg src={require("../../assets/image/" + imgSrc + ".png")} alt="앨범자켓사진" />
-            </GradientEffect>
-            <ProfileGradient
+            </GradientLine>
+            <GradientProfile
               hoverVocalBool={hoverVocal === id}
               clickVocalBool={clickVocal === id}
-              clickVocal={clickVocal}></ProfileGradient>
+              clickVocal={clickVocal}></GradientProfile>
             {play && clickVocal === id && clickVocal !== -1 && (
               <VocalHoverPauseIcon
                 hoverVocalBool={hoverVocal === id}
@@ -82,12 +82,13 @@ export default function VocalList() {
             )}
             {((clickVocal !== id && hoverVocal === id && hoverVocal !== -1) ||
               (!play && clickVocal === id && clickVocal !== -1)) && <VocalHoverPlayIcon />}
-          </MusicProfile>
-          <Hashtags>
+          </MusicProfileWrapper>
+
+          <HashtagUl>
             {hashtags.map((tag, idx) => (
-              <Hashtag key={idx}>#{tag}</Hashtag>
+              <HashtagLi key={idx}>#{tag}</HashtagLi>
             ))}
-          </Hashtags>
+          </HashtagUl>
         </VocalContainer>
       ))}
     </VocalListContainer>
@@ -97,32 +98,42 @@ export default function VocalList() {
 const VocalListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+
   padding-top: 5.6rem;
   padding-left: 9rem;
 `;
 
 const VocalContainer = styled.div`
   display: inline-block;
+
   width: 37.4rem;
   height: 44rem;
-  ${({ theme }) => theme.fonts.body1};
+
   color: ${({ theme }) => theme.colors.white};
+
+  ${({ theme }) => theme.fonts.body1};
 `;
+
 const UsernameInformWrapper = styled.div`
   display: flex;
-  margin-top: 1.8rem;
   position: relative;
   z-index: 1;
+
+  margin-top: 1.8rem;
 `;
 
 const Username = styled.span`
   display: flex;
-  align-items: center;
+
   width: 28.5rem;
-  font-size: 2.4rem;
+
   line-height: 3.1rem;
+  font-size: 2.4rem;
+  align-items: center;
+
   cursor: pointer;
 `;
+
 const VocalSleepIcon = styled(VocalSleepIc)`
   display: block;
 `;
@@ -135,33 +146,40 @@ const CategoryTextWrapper = styled.div``;
 
 const CategoryText = styled.span`
   color: ${({ theme }) => theme.colors.gray3};
+
   margin-right: 0.5rem;
 `;
 
 const CategoryNum = styled.span`
-  color: ${({ theme }) => theme.colors.gray2};
-  background-color: ${({ theme }) => theme.colors.gray5};
-  font-family: "Pretendard";
-  padding: 0.5rem 0.6rem 0.6rem 0.4rem;
+  position: relative;
+  z-index: 1;
+
   border-radius: 50%;
+
+  font-family: "Pretendard";
   font-weight: 400;
   font-size: 1.6rem;
   line-height: 180%;
-  position: relative;
-  z-index: 1;
+
+  padding: 0.5rem 0.6rem 0.6rem 0.4rem;
+
+  color: ${({ theme }) => theme.colors.gray2};
+  background-color: ${({ theme }) => theme.colors.gray5};
 `;
 
 const AlbumCoverImg = styled.img`
   position: relative;
 `;
 
-const ProfileGradient = styled.div<{ hoverVocalBool: boolean; clickVocalBool: boolean; clickVocal: number }>`
+const GradientProfile = styled.div<{ hoverVocalBool: boolean; clickVocalBool: boolean; clickVocal: number }>`
   position: absolute;
-  top: 0;
+
   width: 23.4rem;
   height: 23.4rem;
-  top: 25px;
-  right: 19px;
+
+  top: 2.5rem;
+  right: 1.9rem;
+
   cursor: pointer;
 
   background: linear-gradient(
@@ -183,23 +201,29 @@ const VocalHoverPauseIcon = styled(VocalHoverPauseIc)<{
   display: ${({ hoverVocalBool, clickVocalBool, clickVocal }) =>
     hoverVocalBool || (clickVocalBool && clickVocal !== -1) ? "" : "none"};
   position: absolute;
+
   top: 0;
   margin-left: 10rem;
   margin-top: 10rem;
+
   transform: rotate(-45deg);
+
   cursor: pointer;
 `;
 
 const VocalHoverPlayIcon = styled(VocalHoverPlayIc)`
   position: absolute;
+
   top: 0;
   margin-left: 10rem;
   margin-top: 10rem;
+
   transform: rotate(-45deg);
+
   cursor: pointer;
 `;
 
-const MusicProfile = styled.div<{
+const MusicProfileWrapper = styled.div<{
   hoverVocalBool: boolean;
   clickVocalBool: boolean;
   clickVocal: number;
@@ -207,14 +231,18 @@ const MusicProfile = styled.div<{
 }>`
   position: relative;
   display: inline-block;
+
   width: 28.4rem;
   height: 28.4rem;
-  top: -15px;
-  left: 3px;
+  top: -1.5rem;
+  left: 0.3rem;
+
   transform: rotate(45deg);
+
   border-radius: 5rem;
 
   border: 0.3rem solid transparent;
+
   background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
     linear-gradient(
       to top,
@@ -228,30 +256,41 @@ const MusicProfile = styled.div<{
   background-clip: content-box, border-box;
 `;
 
-const GradientEffect = styled.div`
+const GradientLine = styled.div`
   display: inline-block;
+  overflow: hidden;
+
   width: 23.4rem;
   height: 23.4rem;
-  background-color: ${({ theme }) => theme.colors.sub3};
-  border-radius: 4rem;
-  overflow: hidden;
+
   margin: 2.5rem;
+
+  border-radius: 4rem;
+
+  background-color: ${({ theme }) => theme.colors.sub3};
 `;
 
-const Hashtags = styled.ul`
+const HashtagUl = styled.ul`
   position: relative;
+
   display: flex;
   flex-direction: column-reverse;
   align-items: flex-end;
+
   bottom: 8.5rem;
   right: 6rem;
 `;
-const Hashtag = styled.li`
+
+const HashtagLi = styled.li`
   height: 3.8rem;
+
   padding: 1.7rem 1.5rem;
+  margin-bottom: 1rem;
+
+  line-height: 0.5rem !important;
+
   border-radius: 2.1rem;
+
   background-color: ${({ theme }) => theme.colors.gray5};
   ${({ theme }) => theme.fonts.hashtag};
-  line-height: 0.5rem;
-  margin-bottom: 1rem;
 `;
