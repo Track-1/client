391a11d691ef0b6f06df6d470a2625bda10fc49f
NaveOWO <pianowell96@gmail.com>
2023-01-06 05:26:04 +0900

    [ fix ] 돌림노래 버그 수정 ( 미완 )

--- src/@components/@common/player.tsx
+++ src/@components/@common/player.tsx
@@ -1,122 +1,36 @@
 import styled from "styled-components";
-import ditto from "../../assets/audio/ditto.mp3";
-import { useState, useMemo, useRef, useLayoutEffect, useEffect } from "react";
+import { useState, useLayoutEffect, useRef } from "react";
 import jacketImage from "../../assets/image/thumbnailImg.png";
 import { PauseIc, PlayIc, QuitIc } from "../../assets";
-import { showPlayerBar, playMusic, trackClicked, selectedId } from "../../recoil/player";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { playMusic, showPlayerBar, currentAudioTime } from "../../recoil/player";
 import { tracksOrVocalsCheck } from "../../recoil/tracksOrVocalsCheck";
-import axios from "axios";
+import { useRecoilState, useRecoilValue } from "recoil";
 
-export default function Player() {
+export default function Player(props: any) {
+  const { audio, playAudio, pauseAudio, progress } = props;
+  const duration = parseInt(String(audio.duration / 60)) + ":" + parseInt(String(audio.duration % 60));
+  const tracksOrVocals = useRecoilValue(tracksOrVocalsCheck);
   const playBar = useRef<HTMLDivElement>(null);
-  const [barWidth, setBarWidth] = useState<number>(0);
-  const [progress, setProgress] = useState<number>(0);
-  const [down, setDown] = useState<boolean>(false);
-  const [play, setPlay] = useRecoilState<boolean>(playMusic);
-  const [currentTime, setCurrentTime] = useState<string>("0:0");
 
-  const [id, setId] = useRecoilState<number>(selectedId);
-  const [trackClick, setTrakClick] = useRecoilState<number>(trackClicked);
-  const [audioSrc, setAudioSrc] = useState<string>();
+  const [currentTime, setCurrentTime] = useState<number>(0);
   const [title, setTitle] = useState<string>();
   const [producerName, setProducerName] = useState<string>();
-  const [durationSecond, setDurationSecond] = useState<number>();
+  const [barWidth, setBarWidth] = useState<number>(0);
+  const [down, setDown] = useState<boolean>(false);
 
+  const [play, setPlay] = useRecoilState<boolean>(playMusic);
   const [showPlayer, setShowPlayer] = useRecoilState<boolean>(showPlayerBar);
-
-  const tracksOrVocals = useRecoilValue(tracksOrVocalsCheck);
 
   useLayoutEffect(() => {
     playBar.current && setBarWidth(playBar.current.offsetWidth);
   });
 
-  const audio = useMemo(() => new Audio(audioSrc), [audioSrc]);
-  const duration = parseInt(String(audio.duration / 60)) + ":" + parseInt(String(audio.duration % 60));
-
-  useEffect(() => {
-    getPlayerData();
-    setAudioSrc(ditto);
-    setTitle("Sweet (feat. 구슬한 of 보수동쿨러)");
-    setProducerName("해서웨이(hathaw9y)");
-    setDurationSecond(310);
-
-    if (play) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-
-    audio.addEventListener("timeupdate", () => {
-      setCurrentTime(createCurrentMinuteText(audio.currentTime) + ":" + createCurrentSecontText(audio.currentTime));
-      setProgress((audio.currentTime / audio.duration) * 1000);
-      goProgress();
-    });
-
-    window.addEventListener("keypress", (e: any) => {
-      if (e.keyCode === 32) {
-        e.preventDefault();
-        if (audio.paused) {
-          setPlay(true);
-          audio.play();
-        } else {
-          setPlay(false);
-          audio.pause();
-        }
-        if (audio.currentTime === audio.duration) {
-          audio.play();
-          audio.currentTime = 0;
-          setPlay(true);
-        }
-      }
-    });
-  }, [audio, play, window]);
-
-  function createCurrentMinuteText(currentTime: number) {
-    const minute = parseInt(String(currentTime / 60));
-    return minute < 10 ? `0${minute}` : `${minute}`;
-  }
-
-  function createCurrentSecontText(currentTime: number) {
-    const sec = Math.round(currentTime % 60);
-    return sec < 10 ? `0${sec}` : `${sec}`;
-  }
-
-  async function getPlayerData() {
-    try {
-      const response = await axios.get(`${id}`);
-      setAudioSrc(ditto);
-      setTitle("Sweet (feat. 구슬한 of 보수동쿨러)");
-      setProducerName("해서웨이(hathaw9y)");
-      setDurationSecond(310);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  function playAudio() {
-    setPlay((play) => !play);
-    if (audio.currentTime === audio.duration) {
-      audio.play();
-      audio.currentTime = 0;
-      setPlay(true);
-    }
-  }
-
   function quitAudio() {
     audio.pause();
     audio.currentTime = 0;
 
+    setPlay(false);
     setShowPlayer(false);
-    setPlay((play) => !play);
-    setId(-1);
-    setTrakClick(-1);
-  }
-
-  function goProgress() {
-    const currentDuration = (audio.currentTime / audio.duration) * 100;
-
-    setProgress(currentDuration);
   }
 
   function controlAudio(e: React.MouseEvent<HTMLDivElement>) {
@@ -142,21 +56,10 @@ export default function Player() {
     }
   }
 
-  if (audio.currentTime === audio.duration) {
-    audio.pause();
-    audio.currentTime = 1000;
-    setPlay(false);
-  }
-
   return (
     <PlayerContainer>
-      <PlayerWrapper>
-        <PlayerBarWrapper
-          onClick={controlAudio}
-          onMouseDown={downMouse}
-          onMouseUp={upMouse}
-          onMouseMove={moveAudio}
-          ref={playBar}>
+      <PlayerWrapper onClick={controlAudio} onMouseDown={downMouse} onMouseUp={upMouse} onMouseMove={moveAudio}>
+        <PlayerBarWrapper ref={playBar}>
           <Playbar progress={progress} tracksOrVocals={tracksOrVocals} />
         </PlayerBarWrapper>
 
@@ -168,7 +71,7 @@ export default function Player() {
           <PlayerInformText width={16} whiteText={false}>
             {producerName}
           </PlayerInformText>
-          {play ? <PlayIcon onClick={playAudio} /> : <PauseIcon onClick={playAudio} />}
+          {play ? <PlayIcon onClick={pauseAudio} /> : <PauseIcon onClick={playAudio} />}
           <PlayerInformText width={10} whiteText={true}>
             {currentTime}
           </PlayerInformText>
--- src/@pages/trackSearchPage.tsx
+++ src/@pages/trackSearchPage.tsx
@@ -6,20 +6,58 @@ import TrackListHeader from "../@components/trackSearch/trackListHeader";
 import TrackList from "../@components/trackSearch/trackList";
 import Player from "../@components/@common/player";
 
-import { showPlayerBar } from "../recoil/player";
+import { showPlayerBar, playMusic, currentAudioTime } from "../recoil/player";
 import { tracksOrVocalsCheck } from "../recoil/tracksOrVocalsCheck";
 
 import { useRecoilState, useRecoilValue } from "recoil";
 import { Category } from "../core/common/categoryHeader";
-import { useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+
+import ditto from "../assets/audio/ditto.mp3";
 
 export default function TrackSearchPage() {
+  const audio = useMemo(() => new Audio(ditto), [ditto]);
+
+  const [progress, setProgress] = useState<number>(0);
+
   const showPlayer = useRecoilValue<boolean>(showPlayerBar);
   const [whom, setWhom] = useRecoilState(tracksOrVocalsCheck);
+  const [play, setPlay] = useRecoilState<boolean>(playMusic);
+  const [currentTime, setCurrentTime] = useRecoilState<number>(currentAudioTime);
 
   useEffect(() => {
     setWhom(Category.TRACKS); // 나중에 헤더에서 클릭했을 때도 변경되도록 구현해야겠어요
   }, []);
+
+  function playAudio() {
+    console.log("00", audio.currentTime);
+    audio.play();
+    setPlay(true);
+  }
+
+  useEffect(() => {
+    if (play) {
+      audio.addEventListener("timeupdate", () => {
+        goProgress();
+      });
+    } else {
+      audio.removeEventListener("timeupdate", () => {
+        goProgress();
+      });
+    }
+  }, [audio]);
+
+  function pauseAudio() {
+    console.log(audio.currentTime);
+    audio.pause();
+    setPlay(false);
+  }
+
+  function goProgress() {
+    const currentDuration = (audio.currentTime / audio.duration) * 100;
+
+    setProgress(currentDuration);
+  }
 
   return (
     <>
@@ -30,10 +68,10 @@ export default function TrackSearchPage() {
         </CategoryListWrapper>
         <TrackListWrapper>
           <TrackListHeader />
-          <TrackList />
+          <TrackList audio={audio} playAudio={playAudio} pauseAudio={pauseAudio} />
         </TrackListWrapper>
       </TrackSearchPageWrapper>
-      {showPlayer && <Player />}
+      {showPlayer && <Player audio={audio} playAudio={playAudio} pauseAudio={pauseAudio} progress={progress} />}
     </>
   );
 }
