13aa6974fe47b9923e69c69fa02ef0f44e7c26d0
Seojisoo20191941 <amy0480@naver.com>
2023-01-06 03:32:18 +0900

    [ feat ] 해시태그 구현

--- src/@components/@common/portfoliosInform.tsx
+++ src/@components/@common/portfoliosInform.tsx
@@ -3,7 +3,6 @@ import { PortfolioPropsType,PortfolioType } from "../../type/profilePropsType";
 import {VocalPortfolioTitleTextIc,ProducerPortfolioTitleTextIc} from "../../assets"
 import { useRecoilValue } from "recoil";
 import { tracksOrVocalsCheck } from '../../recoil/tracksOrVocalsCheck';
-import { useState } from 'react';
 
 export default function PortfoliosInform(props:PortfolioPropsType) {
   const isMe=props.isMe;
@@ -15,6 +14,8 @@ export default function PortfoliosInform(props:PortfolioPropsType) {
 
   const tracksOrVocals=useRecoilValue(tracksOrVocalsCheck)
 
+  portfolioInformation&&console.log(portfolioInformation.keyword[0].length)
+
   return (
     <>
     {portfolioInformation&&(
@@ -24,6 +25,9 @@ export default function PortfoliosInform(props:PortfolioPropsType) {
       <InformTitle>{portfolioInformation.title}</InformTitle>
       <InformCategory>{portfolioInformation.category}</InformCategory>
       <InformContent>{portfolioInformation.content}</InformContent>
+      <InformTagWrapper>
+      {portfolioInformation.keyword.map((tag, idx)=>(<InformTag key={idx} textLength={tag.length}>#{tag}</InformTag>))}
+      </InformTagWrapper>
       </InformWrapper>
     )}
     </>
@@ -46,4 +50,27 @@ const InformCategory=styled.p`
 const InformContent=styled.p`
   ${({ theme }) => theme.fonts.description};
   color: ${({ theme }) => theme.colors.gray2};
+`
+
+const InformTag = styled.span<{textLength:number}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 3.8rem;
+  width: ${({textLength})=>textLength+6}rem;
+
+  margin-bottom: 1rem;
+
+  background: ${({ theme }) => theme.colors.gray4};
+  border-radius: 2.1rem;
+
+  ${({ theme }) => theme.fonts.hashtag};
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+
+const InformTagWrapper=styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  column-gap:1rem
 `
