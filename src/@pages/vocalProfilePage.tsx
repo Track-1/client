34280d482126b3d5daabdc468120f8152a901cc1
NaveOWO <pianowell96@gmail.com>
2023-01-07 18:16:22 +0900

    [ fix ] player props 수정

--- src/@pages/vocalProfilePage.tsx
+++ src/@pages/vocalProfilePage.tsx
@@ -4,6 +4,7 @@ import Player from "../@components/@common/player";
 import TracksProfileUploadModal from "../@components/@common/tracksProfileUploadModal";
 import VocalProfileList from "../@components/vocalProfile/vocalProfileList";
 import VocalProfileShadow from "../@components/vocalProfile/vocalProfileShadow";
+import { Category } from "../core/common/categoryHeader";
 import { showPlayerBar } from "../recoil/player";
 import { tracksOrVocalsCheck } from "../recoil/tracksOrVocalsCheck";
 import { uploadButtonClicked } from "../recoil/uploadButtonClicked";
@@ -11,14 +12,14 @@ import { uploadButtonClicked } from "../recoil/uploadButtonClicked";
 export default function VocalProfilePage() {
   const showPlayer = useRecoilValue<boolean>(showPlayerBar);
   const [whom, setWhom] = useRecoilState(tracksOrVocalsCheck);
-  const [visible, setVisible]=useRecoilState<boolean>(uploadButtonClicked)
+  const [visible, setVisible] = useRecoilState<boolean>(uploadButtonClicked);
 
-  setWhom("Vocals");
+  setWhom(Category.VOCALS);
 
   return (
     <>
-      {visible&&<TracksProfileUploadModalSection />}
-      {showPlayer && <Player />}
+      {visible && <TracksProfileUploadModalSection />}
+      {/* {showPlayer && <Player />} */}
       <VocalProfilePageWrapper>
         <VocalProfile>보컬프로필 부분넣어주세요</VocalProfile>
 
@@ -31,10 +32,10 @@ export default function VocalProfilePage() {
   );
 }
 
-const TracksProfileUploadModalSection=styled(TracksProfileUploadModal)`
+const TracksProfileUploadModalSection = styled(TracksProfileUploadModal)`
   position: absolute;
   z-index: 100;
-`
+`;
 
 const VocalProfilePageWrapper = styled.section`
   display: flex;
