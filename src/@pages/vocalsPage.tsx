21e884aa4a0c6ec964c014190ac0d86790517282
[talkingOrange] <sonrisa-bonita@naver.com>
2023-01-07 20:31:46 +0900

    [ etc ] merge로 인한 수정

--- src/@pages/vocalsPage.tsx
+++ src/@pages/vocalsPage.tsx
@@ -25,7 +25,7 @@ export default function VocalsPage() {
     <>
       <CategoryHeader />
 
-      {showPlayer && <Player />}
+      {/* {showPlayer && <Player />} */}
 
       <VocalSearchPageWrapper>
         <CategoryListWrapper>
