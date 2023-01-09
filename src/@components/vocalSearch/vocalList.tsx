9f298da429125c0e00a581f68648a38142df8d85
[talkingOrange] <sonrisa-bonita@naver.com>
2023-01-09 17:57:35 +0900

    [ feat ] test target에 도달 시 observer 실행, pageNum +1

--- src/@components/vocalSearch/vocalList.tsx
+++ src/@components/vocalSearch/vocalList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -13,14 +13,20 @@ export default function VocalList() {
   const [showPlayer, setShowPlayer] = useRecoilState<boolean>(showPlayerBar);
   const [play, setPlay] = useRecoilState<boolean>(playMusic);
   const [beatId, setBeatId] = useRecoilState<number>(selectedId);
+
   const [vocalData, setVocalData] = useState<VocalSearchType[]>();
+  const [page, setPage] = useState(1);
+  // const [loading, setLoading] = useState(false);
+  const loadMore = () => setPage((prev) => prev + 1);
+  const target = useRef<HTMLDivElement | null>(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     getVocalsData().then((result) => result && setVocalData(result.data));
-  }, []);
-
+  }, [page]);
+  console.log(page);
+  console.log(vocalData);
   function mouseOverPlayVocal(id: number) {
     setHoverVocal(id);
   }
@@ -45,7 +51,16 @@ export default function VocalList() {
   function clickVocalName(id: number) {
     navigate("/vocal-profile", { state: id });
   }
-
+  useEffect(() => {
+    //  if (loading) {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadMore();
+      }
+    });
+    observer.observe(target.current!);
+    // }
+  }, []);
   return (
     <VocalListContainer>
       {vocalData &&
@@ -100,6 +115,7 @@ export default function VocalList() {
             </HashtagUl>
           </VocalContainer>
         ))}
+      <InfiniteDiv ref={target}> 아아 </InfiniteDiv>
     </VocalListContainer>
   );
 }
@@ -107,19 +123,15 @@ export default function VocalList() {
 const VocalListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-
   padding-top: 4rem;
   padding-left: 9rem;
 `;
 
 const VocalContainer = styled.div`
   display: inline-block;
-
   width: 37.4rem;
   height: 44rem;
-
   color: ${({ theme }) => theme.colors.white};
-
   ${({ theme }) => theme.fonts.body1};
 `;
 
@@ -127,21 +139,16 @@ const UsernameInformWrapper = styled.div`
   display: flex;
   position: relative;
   z-index: 1;
-
   margin-top: 1.8rem;
 `;
 
 const Username = styled.span`
   display: flex;
-
   line-height: 3.1rem;
   font-size: 2.4rem;
   align-items: center;
-
   margin-bottom: 1.1rem;
-
   cursor: pointer;
-
   :hover {
     color: ${({ theme }) => theme.colors.sub2};
   }
@@ -150,7 +157,6 @@ const Username = styled.span`
 const VocalSleepIcon = styled(VocalSleepIc)`
   display: block;
   position: absolute;
-
   right: 6.2rem;
 `;
 
@@ -158,23 +164,18 @@ const CategoryTextWrapper = styled.div``;
 
 const CategoryText = styled.span`
   color: ${({ theme }) => theme.colors.gray3};
-
   margin-right: 0.5rem;
 `;
 
 const CategoryNum = styled.span`
   position: relative;
   z-index: 1;
-
   border-radius: 50%;
-
   font-family: "Pretendard";
   font-weight: 400;
   font-size: 1.6rem;
   line-height: 180%;
-
   padding: 0.5rem 0.6rem 0.6rem 0.4rem;
-
   color: ${({ theme }) => theme.colors.gray2};
   background-color: ${({ theme }) => theme.colors.gray5};
 `;
@@ -185,15 +186,11 @@ const AlbumCoverImg = styled.img`
 
 const GradientProfile = styled.div<{ isHoverVocal: boolean; isClickVocal: boolean; clickVocal: number }>`
   position: absolute;
-
   width: 23.4rem;
   height: 23.4rem;
-
   top: 2.5rem;
   right: 1.9rem;
-
   cursor: pointer;
-
   background: linear-gradient(
     135deg,
     ${({ theme }) => theme.colors.sub3} 15.32%,
@@ -213,25 +210,19 @@ const VocalHoverPauseIcon = styled(VocalHoverPauseIc)<{
   display: ${({ isHoverVocal, isClickVocal, clickVocal }) =>
     isHoverVocal || (isClickVocal && clickVocal !== -1) ? "" : "none"};
   position: absolute;
-
   top: 0;
   margin-left: 10rem;
   margin-top: 10rem;
-
   transform: rotate(-45deg);
-
   cursor: pointer;
 `;
 
 const VocalHoverPlayIcon = styled(VocalHoverPlayIc)`
   position: absolute;
-
   top: 0;
   margin-left: 10rem;
   margin-top: 10rem;
-
   transform: rotate(-45deg);
-
   cursor: pointer;
 `;
 
@@ -243,18 +234,13 @@ const MusicProfileWrapper = styled.div<{
 }>`
   position: relative;
   display: inline-block;
-
   width: 28.4rem;
   height: 28.4rem;
   top: -1.5rem;
   left: 0.3rem;
-
   transform: rotate(45deg);
-
   border-radius: 5rem;
-
   border: 0.3rem solid transparent;
-
   background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
     linear-gradient(
       to top,
@@ -271,38 +257,34 @@ const MusicProfileWrapper = styled.div<{
 const GradientLine = styled.div`
   display: inline-block;
   overflow: hidden;
-
   width: 23.4rem;
   height: 23.4rem;
-
   margin: 2.5rem;
-
   border-radius: 4rem;
-
   background-color: ${({ theme }) => theme.colors.sub3};
 `;
 
 const HashtagUl = styled.ul`
   position: relative;
-
   display: flex;
   flex-direction: column-reverse;
   align-items: flex-end;
-
   bottom: 8.5rem;
   right: 7.5rem;
 `;
 
 const HashtagLi = styled.li`
   height: 3.8rem;
-
   padding: 1.7rem 1.5rem;
   margin-bottom: 1rem;
-
   line-height: 0.3rem !important;
-
   border-radius: 2.1rem;
-
   background-color: ${({ theme }) => theme.colors.gray5};
   ${({ theme }) => theme.fonts.hashtag};
+`;
+
+const InfiniteDiv = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: pink;
 `;
