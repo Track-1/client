3fd13a330b501dd6141853d70aaefb58f5732b02
Seojisoo20191941 <amy0480@naver.com>
2023-01-09 17:42:34 +0900

    [ etc ] 라이브러리 설치

--- src/mocks/handlers/producerProfile.ts
+++ src/mocks/handlers/producerProfile.ts
@@ -8,7 +8,7 @@ export const producerProfileHandler = [
       ctx.status(200),
       ctx.json([
         {
-          isMe: true,
+          isMe: false,
           producerProfile: {
             profileImage: portFolioImg,
             name: "프로듀서1",
