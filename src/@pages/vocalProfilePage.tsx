03ad58c19cc8774c89182e77300eb1ca7d66b1ee
[talkingOrange] <sonrisa-bonita@naver.com>
2023-01-12 04:05:04 +0900

    [ feat ] vocalProfilePage 무한 스크롤

--- src/@pages/vocalProfilePage.tsx
+++ src/@pages/vocalProfilePage.tsx
@@ -1,4 +1,5 @@
 import { useRecoilState, useRecoilValue } from "recoil";
+import axios from "axios";
 import styled from "styled-components";
 import Player from "../@components/@common/player";
 import TracksProfileUploadModal from "../@components/@common/tracksProfileUploadModal";
@@ -8,7 +9,7 @@ import { Category } from "../core/common/categoryHeader";
 import { showPlayerBar, playMusic } from "../recoil/player";
 import { tracksOrVocalsCheck } from "../recoil/tracksOrVocalsCheck";
 import { uploadButtonClicked } from "../recoil/uploadButtonClicked";
-import { useMemo, useState, useEffect } from "react";
+import { useMemo, useState, useEffect, useRef, useCallback } from "react";
 import { getVocalProfile } from "../core/api/vocalProfile";
 import { VocalPortfolioType, VocalProfileType } from "../type/vocalProfile";
 import { useQuery } from "react-query";
@@ -23,40 +24,81 @@ export default function VocalProfilePage() {
   const [duration, setCurrentDuration] = useState<number>(0);
   const [isMe, setIsMe] = useState<boolean>(false);
   const [profileData, setProfileData] = useState<VocalProfileType>();
-  const [portfolioData, setPortfolioData] = useState<VocalPortfolioType[]>();
+  const [portfolioData, setPortfolioData] = useState<VocalPortfolioType[]>([]);
 
   const audio = useMemo(() => new Audio(), []);
 
-  const {state}=useLocation()
+  const { state } = useLocation();
+
+  // infinite
+  const targetRef = useRef<any>();
+  const page = useRef<number>(1);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+
+  const fetch = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/profile/producer/2?page=${page.current}&limit=3`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_PRODUCER_ACCESSTOKEN}`,
+          },
+        },
+      );
+      setPortfolioData((prev) => prev && [...prev, ...data?.data?.producerPortfolio]);
+      console.log(page.current);
+      console.log(portfolioData);
+
+      setHasNextPage(data?.data.producerPortfolio.length === 4);
+      if (data?.data.producerPortfolio.length) {
+        page.current += 1;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    // if (!targetRef.current || !hasNextPage) return;
+    const io = new IntersectionObserver((entries, observer) => {
+      if (entries[0].isIntersecting) {
+        fetch();
+      }
+    });
+    io.observe(targetRef.current);
+
+    return () => {
+      io.disconnect();
+    };
+  }, [fetch, hasNextPage]);
+
+  //end
 
   useEffect(() => {
     setWhom(Category.VOCALS);
   }, []);
 
-
-  const { data } = useQuery(["state",state], ()=>getVocalProfile(state)
-  , {
-    refetchOnWindowFocus: false, 
-    retry: 0, 
-    onSuccess: data => {
+  const { data } = useQuery(["state", state], () => getVocalProfile(state), {
+    refetchOnWindowFocus: false,
+    retry: 0,
+    onSuccess: (data) => {
       if (data?.status === 200) {
         console.log(data);
         console.log("성공");
         setIsMe(data?.data.data.isMe);
         setProfileData(data?.data.data.vocalProfile);
-        setPortfolioData(data?.data.data.vocalPortfolio);  
-      }    
+        setPortfolioData(data?.data.data.vocalPortfolio);
+      }
     },
-    onError: error => {
+    onError: (error) => {
       console.log("실패");
-    }
+    },
   });
 
   function playAudio() {
     audio.play();
     setPlay(true);
   }
-
 
   // function playAudio() {
   //   audio.play();
@@ -99,7 +141,6 @@ export default function VocalProfilePage() {
       {visible && <TracksProfileUploadModalSection />}
       <VocalProfilePageWrapper>
         <VocalProfile>보컬프로필 부분넣어주세요</VocalProfile>
-
         <VocalProfileWrapper>
           {portfolioData && (
             <VocalProfileList
@@ -116,12 +157,21 @@ export default function VocalProfilePage() {
         </VocalProfileWrapper>
       </VocalProfilePageWrapper>
       <PlayerWrapper></PlayerWrapper>
+      <InfiniteDiv ref={targetRef}> 아아이 </InfiniteDiv>
+
       {showPlayer && (
         <Player audio={audio} playAudio={playAudio} pauseAudio={pauseAudio} progress={progress} duration={duration} />
       )}
     </>
   );
 }
+
+// infinite
+const InfiniteDiv = styled.div`
+  width: 100%;
+  height: 1rem;
+  background-color: red;
+`;
 
 const TracksProfileUploadModalSection = styled(TracksProfileUploadModal)`
   position: absolute;
