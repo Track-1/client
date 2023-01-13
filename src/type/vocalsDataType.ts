28389e611c43286879c392471cb3869e5d5932bc
[talkingOrange] <sonrisa-bonita@naver.com>
2023-01-14 03:18:41 +0900

    [ fix ] vocal 화면 에러 해결

---
+++ src/type/vocalsDataType.ts
@@ -0,0 +1,11 @@
+export interface VocalsDataType {
+  category: string[];
+  isSelected: boolean;
+  keyword: string[];
+  totalCategNum: number;
+  vocalId: number;
+  vocalName: string;
+  vocalProfileImage: string;
+  vocalTitleFile: string;
+  wavFileLength: number;
+}
