50bf57a57952775b33171bdf853c72cc15ad8d87
NaveOWO <pianowell96@gmail.com>
2023-01-30 16:03:21 +0900

    [ fix ] 페이지별 wavFile 이름 다른 오류 수정

--- src/utils/hooks/usePlay.ts
+++ src/utils/hooks/usePlay.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { playMusic, showPlayerBar } from "../../recoil/player";
 
-export default function usePlay(audio: any, data: any) {
+export default function usePlay(audio: any, data: any, pageCategory: string) {
   const [play, setPlay] = useRecoilState(playMusic);
   const [showPlayer, setShowPlayer] = useRecoilState(showPlayerBar);
   const [clickedIndex, setClickedIndex] = useState<number>(-1);
@@ -14,8 +14,25 @@ export default function usePlay(audio: any, data: any) {
   }, [currentFile]);
 
   useEffect(() => {
-    setCurrentFile(data[clickedIndex]?.wavFile);
-    audio.src = data[clickedIndex]?.wavFile;
+    switch (pageCategory) {
+      case "tracks":
+        setCurrentFile(data[clickedIndex]?.wavFile);
+        audio.src = data[clickedIndex]?.wavFile;
+        break;
+      case "vocals":
+        setCurrentFile(data[clickedIndex]?.vocalTitleFile);
+        audio.src = data[clickedIndex]?.vocalTitleFile;
+        break;
+      case "profile":
+        setCurrentFile(data[clickedIndex]?.beatWavFile);
+        audio.src = data[clickedIndex]?.beatWavFile;
+        break;
+      case "comments":
+        console.log(data);
+        setCurrentFile(data[clickedIndex]?.vocalWavFile);
+        audio.src = data[clickedIndex]?.vocalWavFile;
+        break;
+    }
   }, [clickedIndex]);
 
   function playAudio(currentIndex: number) {
@@ -25,5 +42,5 @@ export default function usePlay(audio: any, data: any) {
     clickedIndex === currentIndex ? audio.play() : setClickedIndex(currentIndex);
   }
 
-  return { clickedIndex, playAudio };
+  return { clickedIndex, setClickedIndex, playAudio };
 }
