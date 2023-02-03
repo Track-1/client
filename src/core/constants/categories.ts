a6159089ae74490d7b8d858fc89b29f370c51e92
myeongheonhong <ruddyh7076@gmail.com>
2023-02-03 10:33:46 +0900

    [ refactor ] 불필요 코드 삭제 & 데이터 순회로 로직 변경

---
+++ src/core/constants/categories.ts
@@ -0,0 +1,11 @@
+export enum Categories {
+  "R&B" = "R&B",
+  HIPHOP = "Hiphop",
+  BALLAD = "Ballad",
+  POP = "Pop",
+  ROCK = "Rock",
+  EDM = "EDM",
+  JAZZ = "JAZZ",
+  HOUSE = "House",
+  FUNK = "Funk",
+}
