cae9b314c7acda69951b1bff8cd7d1008c894b82
Seojisoo20191941 <amy0480@naver.com>
2023-02-09 12:00:12 +0900

    [ design ] 버튼 및 백그라운드 마진값 조정

--- src/@pages/signUpPage.tsx
+++ src/@pages/signUpPage.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import styled from 'styled-components';
 import BackButton from '../@components/@common/backButton';
 import { SignBackgroundIc } from '../assets';
 
 export default function signUpPage() {
   return (
     <>
-        <BackButton/>
-        <SignBackgroundIc/>
+        <BackButtonWrapper>
+            <BackButton/>
+        </BackButtonWrapper>
+        <SignBackgroundIcon/>
     </>
   )
 }
+
+const BackButtonWrapper=styled.div`
+    margin: 5.9rem 0 0 7.9rem;
+`
+
+const SignBackgroundIcon=styled(SignBackgroundIc)`
+    margin-top: 26.6rem;
+`
