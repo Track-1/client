85638b44a61255bdf498bb5ac22ca3f0cc7574fe
Seojisoo20191941 <amy0480@naver.com>
2023-02-11 05:37:58 +0900

    [ feat ] 로그인 네비게이션 추가

--- src/@components/signUp/signupStepHeader.tsx
+++ src/@components/signUp/signupStepHeader.tsx
@@ -2,17 +2,27 @@ import React from 'react'
 import { LoginHereMessageIc, SignUpStep1Ic, SignUpStep2Ic, SignUpStep3Ic, SignUpStepBlanckIc } from '../../assets';
 import { StepPropsType } from '../../type/signUpStepTypes'
 import { chaeckStepType } from '../../utils/signUp/stepType';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function SignupStepHeader(props:StepPropsType) {
     const {step}=props;
+    const navigate=useNavigate()
+
+    function moveLoginPage(){
+        navigate('/log-in')
+    }
 
   return (
     <>
         {chaeckStepType(step)===1?<SignUpStep1Ic/>:<SignUpStepBlanckIc/>}
         {chaeckStepType(step)===2?<SignUpStep2Ic/>:<SignUpStepBlanckIc/>}
         {chaeckStepType(step)===3?<SignUpStep3Ic/>:<SignUpStepBlanckIc/>}
-        <LoginHereMessageIc/>
+        <LoginHereMessageIcon onClick={moveLoginPage}/>
     </>
   )
 }
+
+const LoginHereMessageIcon=styled(LoginHereMessageIc)`
+    cursor: pointer;
+`
