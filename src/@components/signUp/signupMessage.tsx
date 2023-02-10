ee33f82384357be970f02557ff7d7f001e8e4d91
Seojisoo20191941 <amy0480@naver.com>
2023-02-11 05:18:34 +0900

    [ design ] 회원가입 메시지 마진값 조정

--- src/@components/signUp/signupMessage.tsx
+++ src/@components/signUp/signupMessage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import styled from 'styled-components';
 import { SignBgLogoIc, SignWelcomeIc } from '../../assets'
 import { StepPropsType } from '../../type/signUpStepTypes'
 import { isMessageLogo,isMessageWelcome } from '../../utils/signUp/checkMessageType';
@@ -8,8 +8,16 @@ export default function SignupMessage(props:StepPropsType) {
 
   return (
     <>
-    {isMessageLogo(step)&&<SignBgLogoIc/>}
-    {isMessageWelcome(step)&&<SignWelcomeIc/>}
+    {isMessageLogo(step)&&<SignBgLogoIcon/>}
+    {isMessageWelcome(step)&&<SignWelcomeIcon/>}
     </>
   )
 }
+
+const SignBgLogoIcon=styled(SignBgLogoIc)`
+    margin: 53.4rem 0 0 23.3rem;
+`
+
+const SignWelcomeIcon=styled(SignWelcomeIc)`
+    margin: 45.14rem 0 0 32.7rem;
+`
