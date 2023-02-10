e1f4b57a7c5d21d2ae8c7dd04e202105e766e8bf
Seojisoo20191941 <amy0480@naver.com>
2023-02-11 06:37:59 +0900

    [ feat ] 역할 호버 이벤트 구현

--- src/@components/signUp/signupRole.tsx
+++ src/@components/signUp/signupRole.tsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
 import styled from 'styled-components'
 import { SignupNotSelectedProducerIc, SignupNotSelectedVocalIc, SignupSelectedProducerIc, SignupSelectedVocalIc, SignUpSelectRoleTitleIc } from '../../assets'
+import { currentUser } from '../../core/constants/userType';
 import { SetStepPropsType } from '../../type/signUpStepTypes';
 
 export default function SignupRole(props:SetStepPropsType) {
     const {setStep}=props;
+    const [hoveredRole, setHoveredRole] = useState<string>('')
+
+    function hoverRole(role:string){
+      setHoveredRole(role)
+    }
+
+    function hoverOut(){
+      setHoveredRole('')
+    }
+
+    function isProducerHovered(){
+      return hoveredRole===currentUser.PRODUCER
+    }
+
+    function isVocalHovered(){
+      return hoveredRole===currentUser.VOCAL
+    }
 
   return (
     <>
     <SignUpSelectRoleTitleIc/>
-    <SignupNotSelectedProducerIc/>
-    <SignupNotSelectedVocalIc/>
-    <SignupSelectedProducerIc/>
-    <SignupSelectedVocalIc/>
+
+    {isProducerHovered()?<SignupSelectedProducerIc/>:<SignupNotSelectedProducerIc onMouseEnter={()=>hoverRole(currentUser.PRODUCER)} onMouseOut={hoverOut}/>}
+    {isVocalHovered()?<SignupSelectedVocalIc/>:<SignupNotSelectedVocalIc onMouseEnter={()=>hoverRole(currentUser.VOCAL)} onMouseOut={hoverOut}/>}    
+    
     </>
   )
 }
