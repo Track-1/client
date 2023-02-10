6a1936f0fe32f105e1727613403df501c2ef1cdc
Seojisoo20191941 <amy0480@naver.com>
2023-02-11 06:53:28 +0900

    [ feat ] 역할 클릭 이벤트 구현

--- src/@components/signUp/signupRole.tsx
+++ src/@components/signUp/signupRole.tsx
@@ -7,30 +7,64 @@ import { SetStepPropsType } from '../../type/signUpStepTypes';
 export default function SignupRole(props:SetStepPropsType) {
     const {setStep}=props;
     const [hoveredRole, setHoveredRole] = useState<string>('')
+    const [selectedRole, setSelectedRole] = useState<string>('')
 
     function hoverRole(role:string){
       setHoveredRole(role)
     }
 
     function hoverOut(){
-      setHoveredRole('')
+      setHoveredRole("not")
     }
 
-    function isProducerHovered(){
-      return hoveredRole===currentUser.PRODUCER
+    function selectRole(role:string){
+      setSelectedRole(role)
     }
 
-    function isVocalHovered(){
-      return hoveredRole===currentUser.VOCAL
+    function checkHoveredRole(role:string){
+      return hoveredRole===role
     }
+
+    function checkSelectedRole(role:string){
+      return selectedRole===role
+    }
+
+    console.log(hoveredRole)
 
   return (
-    <>
-    <SignUpSelectRoleTitleIc/>
+    <RoleWrapper>
+      <SignUpSelectRoleTitleIc/>
 
-    {isProducerHovered()?<SignupSelectedProducerIc/>:<SignupNotSelectedProducerIc onMouseEnter={()=>hoverRole(currentUser.PRODUCER)} onMouseOut={hoverOut}/>}
-    {isVocalHovered()?<SignupSelectedVocalIc/>:<SignupNotSelectedVocalIc onMouseEnter={()=>hoverRole(currentUser.VOCAL)} onMouseOut={hoverOut}/>}    
+      {checkHoveredRole(currentUser.PRODUCER)||checkSelectedRole(currentUser.PRODUCER)?<SignupSelectedProducerIcon onClick={()=>selectRole(currentUser.PRODUCER)} onMouseEnter={()=>hoverRole(currentUser.PRODUCER)} onMouseOut={hoverOut}/>:<SignupNotSelectedProducerIcon onMouseEnter={()=>hoverRole(currentUser.PRODUCER)} onMouseOut={hoverOut}/>}
+      {checkHoveredRole(currentUser.VOCAL)||checkSelectedRole(currentUser.VOCAL)?<SignupSelectedVocalIcon onClick={()=>selectRole(currentUser.VOCAL)} onMouseEnter={()=>hoverRole(currentUser.VOCAL)} onMouseOut={hoverOut}/>:<SignupNotSelectedVocalIcon onMouseEnter={()=>hoverRole(currentUser.VOCAL)} onMouseOut={hoverOut}/>}    
     
-    </>
+    </RoleWrapper>
   )
 }
+
+const RoleWrapper=styled.section`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 7.7rem;
+`
+
+const SignupSelectedProducerIcon=styled(SignupSelectedProducerIc)`
+  margin:3.3rem 0 1.4rem 0;
+
+  cursor: pointer;
+`
+
+const SignupNotSelectedProducerIcon=styled(SignupNotSelectedProducerIc)`
+  margin:3.3rem 0 1.4rem 0;
+
+  cursor: pointer;
+`
+
+const SignupSelectedVocalIcon=styled(SignupSelectedVocalIc)`
+  cursor: pointer;
+`
+
+const SignupNotSelectedVocalIcon=styled(SignupNotSelectedVocalIc)`
+  cursor: pointer;
+`
