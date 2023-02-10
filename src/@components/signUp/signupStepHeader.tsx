512fc1e6da9da4f3b32f434fe9126daf78c8ae31
Seojisoo20191941 <amy0480@naver.com>
2023-02-11 05:50:16 +0900

    [ design ] 회원가입 스텝 헤더 스타일링

--- src/@components/signUp/signupStepHeader.tsx
+++ src/@components/signUp/signupStepHeader.tsx
@@ -14,14 +14,14 @@ export default function SignupStepHeader(props:StepPropsType) {
     }
 
   return (
-    <>
+    <StepHeaderWrapper>
         <StepsWrapper>
             {chaeckStepType(step)===1?<SignUpStep1Ic/>:<SignUpStepBlanckIc/>}
             {chaeckStepType(step)===2?<SignUpStep2Ic/>:<SignUpStepBlanckIc/>}
             {chaeckStepType(step)===3?<SignUpStep3Ic/>:<SignUpStepBlanckIc/>}
         </StepsWrapper>
         <LoginHereMessageIcon onClick={moveLoginPage}/>
-    </>
+    </StepHeaderWrapper>
   )
 }
 
@@ -29,9 +29,20 @@ const LoginHereMessageIcon=styled(LoginHereMessageIc)`
     cursor: pointer;
 `
 
-const StepsWrapper=styled.section`
+const StepsWrapper=styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
 
     width: 11.2rem;
+    height: 2.4rem;
+`
+
+const StepHeaderWrapper=styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 56rem;
+    height: 2.4rem;
 `
