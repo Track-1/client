6542b9489d8789b8a21997b0724d155065e0fd0d
Seojisoo20191941 <amy0480@naver.com>
2023-02-14 07:53:44 +0900

    [ feat ] 이메일 부분 제목 삽입

--- src/@components/signUp/signupEmail.tsx
+++ src/@components/signUp/signupEmail.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { WhatsYourEmailIc } from '../../assets';
+import styled from 'styled-components';
+import { SignUpEmailTitleIc, SignUpPasswordIc, WhatsYourEmailIc } from '../../assets';
 import { SetStepPropsType } from '../../type/signUpStepTypes';
 
 export default function SignupEmail(props:SetStepPropsType) {
     const {setStep}=props;
 
   return (
+    <SignupEmailWrapper>
+    <SignUpEmailTitleIc/>
     <WhatsYourEmailIc/>
+    <SignUpPasswordIc/>
+    </SignupEmailWrapper>
   )
 }
+
+const SignupEmailWrapper=styled.section`
+    display: flex;
+    flex-direction: column;
+`
