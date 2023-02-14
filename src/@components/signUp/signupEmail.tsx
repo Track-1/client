f0e2cb7cdeb71d4ef012cd72a175a2454335c936
Seojisoo20191941 <amy0480@naver.com>
2023-02-14 12:03:20 +0900

    [ design ] input 창 스타일링

--- src/@components/signUp/signupEmail.tsx
+++ src/@components/signUp/signupEmail.tsx
@@ -22,9 +22,9 @@ export default function SignupEmail(props:SetStepPropsType) {
         <SignUpEmailTitleIcon/>
         <SignupEmailWrapper>
             <WhatsYourEmailIcon/>
-            <Input type="email" placeholder="Enter your email address" onChange={writeEmail}/>
+            <Input type="email" placeholder="Enter your email address" width={42.2} onChange={writeEmail}/>
             <SignUpPasswordIcon/>
-            <Input type="email" placeholder="Create a password" onChange={writePassword}/>
+            <Input type="password" placeholder="Create a password" width={56} onChange={writePassword}/>
         </SignupEmailWrapper>
     </>
   )
@@ -49,8 +49,21 @@ const SignupEmailWrapper=styled.div`
     margin-left:11rem;
 `
 
-const Input=styled.input`
+const Input=styled.input<{width:number}>`
+    display: flex;
+    align-items: center;
+
+    padding: 3rem 0 0.5rem 0;
+
+    width: ${({width})=>width}rem;
+    
+    border-bottom: 0.1rem solid ${({ theme }) => theme.colors.gray3};
+
     color: ${({ theme }) => theme.colors.white};
 
     ${({ theme }) => theme.fonts.input};
+
+    &::placeholder{
+        color: ${({ theme }) => theme.colors.gray4};
+    }
 `
