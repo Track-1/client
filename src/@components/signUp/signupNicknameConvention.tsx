d638cd1a07b04075d8123c4d35dc8ee21ac83a2f
Seojisoo20191941 <amy0480@naver.com>
2023-02-26 19:10:22 +0900

    [ design ] 프로필 이미지 업로드 스타일 구현

--- src/@components/signUp/signupNicknameConvention.tsx
+++ src/@components/signUp/signupNicknameConvention.tsx
@@ -3,6 +3,7 @@ import { SignUpUploadImageIc } from '../../assets';
 import { SetStepPropsType } from '../../type/signUpStepTypes';
 import { UploadInfoDataType } from '../../type/uploadInfoDataType';
 import { uploadImage } from '../../utils/uploadPage/uploadImage';
+import styled from 'styled-components';
 
 export default function SignupNicknameConvention(props:SetStepPropsType) {
     const {setStep}=props;
@@ -19,11 +20,39 @@ export default function SignupNicknameConvention(props:SetStepPropsType) {
 
   return (
     <>
-      <input type="file" onChange={(e) => {uploadImage(e)}} />
-      <div className="preview">
-        {imageSrc && <img src={imageSrc} alt="preview-img" />}
-      </div>
+    <ImageContainer>
+      <ImgWrapper>
+          {imageSrc && <Img src={imageSrc} alt="preview-img" />}
+        </ImgWrapper>
+
+      <label htmlFor="profile-img">
+        <SignUpUploadImageIcon/>
+      </label>
+        <input type="file" id="profile-img" style={{ visibility: "hidden" }} onChange={(e) => {uploadImage(e)}} />
+    </ImageContainer>
     </>
   );
 
 }
+
+const SignUpUploadImageIcon=styled(SignUpUploadImageIc)`
+  cursor: pointer;
+`
+
+const ImageContainer=styled.section`
+  margin: 6.4rem 28.1rem 4.1rem 28.1rem;
+`
+
+const ImgWrapper=styled.div`
+  width: 21.7rem;
+  height: 21.7rem;
+  border-radius: 25rem;
+
+  position: absolute;
+
+  overflow: hidden;
+`
+
+const Img=styled.img`
+  width: 100%;
+`
