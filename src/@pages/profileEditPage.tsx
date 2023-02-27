86bae6a69d8847e3cb2254cc129aa27b5ff19fb2
NaveOWO <pianowell96@gmail.com>
2023-02-27 17:49:23 +0900

    [ feat ] : 프로필사진 & 닉네임 입력화면 구현

---
+++ src/@pages/profileEditPage.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import ProfileEditInfo from "../@components/@common/profileEditInfo";
+import ProfileEditHeader from "../@components/profileEdit/profileEditHeader";
+import ProfileEditTitle from "../@components/profileEdit/profileEditTitle";
+import { editInputDatas } from "../core/editProfile/editData";
+import { EditDataType } from "../type/editDataType";
+
+export default function ProfileEditPage() {
+  const [editDatas, setEditDatas] = useState<EditDataType>(editInputDatas);
+  const [isSave, setIsSave] = useState<boolean>(false);
+  const [isMeetRequired, setIsMeetRequired] = useState<boolean>(false);
+  function editDats(datas: any) {
+    setEditDatas(datas);
+  }
+
+  function saveEditDatas() {
+    setIsSave(true);
+  }
+
+  function activeSaveButton(inputState: string) {
+    inputState === "correct" ? setIsMeetRequired(true) : setIsMeetRequired(false);
+  }
+
+  return (
+    <>
+      <ProfileEditHeader saveEditDatas={saveEditDatas} isMeetRequired={isMeetRequired} />
+      <EditContainer>
+        <ProfileEditTitle activeSaveButton={activeSaveButton} />
+        <ProfileEditInfo isSave={isSave} editDatas={editDats} />
+      </EditContainer>
+    </>
+  );
+}
+
+const EditContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0 21.8rem;
+`;
