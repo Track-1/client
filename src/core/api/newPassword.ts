cdfd50c806a794c576375c32f3ab618ef5e5c4f1
myeongheonhong <ruddyh7076@gmail.com>
2023-03-05 22:25:09 +0900

    [ feat ] 비밀번호 찾기 이메일전송 커스텀훅 분리

--- src/core/api/newPassword.ts
+++ src/core/api/newPassword.ts
@@ -1,17 +1,14 @@
 import axios from "axios";
 
 export async function postNewPassword(tableName: string, userEmail: string) {
-  try {
-    await axios.post(`${process.env.REACT_APP_BASE_URL}/user/etc/newpassword-mail`, {
-      header: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: {
-        tableName: tableName,
-        userEmail: userEmail,
-      },
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  const body = {
+    tableName: tableName,
+    userEmail: userEmail,
+  };
+
+  return await axios.post(`${process.env.REACT_APP_BASE_URL}/user/etc/newpassword-mail`, JSON.stringify(body), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
