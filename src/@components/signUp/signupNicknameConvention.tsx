01328395ddf1fafb3bd7da9fe59a27f9f616ba32
Seojisoo20191941 <amy0480@naver.com>
2023-03-09 19:43:55 +0900

    [ etc ] 불필요 코드 삭제

--- src/@components/signUp/signupNicknameConvention.tsx
+++ src/@components/signUp/signupNicknameConvention.tsx
@@ -145,58 +145,6 @@ export default function SignupNicknameConvention(props:SetUserPropsType) {
 
 }
 
-const Label=styled.label`
-  cursor: pointer;
-`
-
-const ImageContainer=styled.section`
-  margin: 6.4rem 28.1rem 4.1rem 28.1rem;
-  width: 21.7rem;
-  height: 21.7rem;
-`
-
-const ImgWrapper=styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  width: 21.7rem;
-  height: 21.7rem;
-
-  border-radius: 25rem;
-
-  position: absolute;
-  overflow: hidden;
-
-`
-
-const Img=styled.img`
-    position: absolute;
-    top: 0;
-    left: 0;
-    transform: translate(50, 50);
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    margin: auto;
-`
-
-const SignUpChangeImageIcon=styled(SignUpChangeImageIc)`
-  width: 21.7rem;
-  height: 21.7rem;
-
-  border: 0.1rem solid rgba(30, 32, 37, 0.5);
-  border-radius: 25rem;
-
-  position: relative;
-  
-  backdrop-filter: blur(1.7rem);
-`
-
-const SignUpUploadImageIcon=styled(SignUpUploadImageIc)`
-  position: absolute;
-`
-
 const Input=styled.input<{width:number, underline:string}>`
     display: flex;
     align-items: center;
