3a876de7c0922af003bada60226fc3990a751679
myeongheonhong <ruddyh7076@gmail.com>
2023-03-13 00:20:22 +0900

    [ feat ] token 방법 변경

--- src/core/api/resetPassword.ts
+++ src/core/api/resetPassword.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { getCookie } from "../../utils/cookie";
 
 export async function patchResetPassword(password: string) {
-  const token = localStorage.getItem("token");
+  const token = getCookie("forgotPasswordToken");
   try {
     const data = await axios.patch(`${process.env.REACT_APP_BASE_URL}/user/etc/newpassword/${token}`, password, {
       headers: {
