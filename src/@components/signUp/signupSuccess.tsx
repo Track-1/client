dd5baaad5f4f3f8fe2c32eccbf2e55380b8d2d15
Seojisoo20191941 <amy0480@naver.com>
2023-03-16 02:11:20 +0900

    [ feat ] 모달창 바깥 클릭 시 꺼지도록 구현

--- src/@components/signUp/signupSuccess.tsx
+++ src/@components/signUp/signupSuccess.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import { MoveTouploadPortfolioButtonIc, MoveTouploadVocalSearchingButtonIc, SignUpGetStartedButtonIc, SignUpProducerButtonIc, SignUpProducerQIc, SignUpSuccessBackgroundIc, SignUpVocalButtonIc, SignUpVocalQIc } from '../../assets'
 import { useNavigate } from 'react-router-dom';
@@ -8,11 +8,13 @@ import { currentUser } from '../../core/constants/userType';
 import { profileCategory } from '../../core/constants/pageCategory';
 import { useState } from 'react';
 import { isProducer, isVocal } from '../../utils/common/userType';
+import useModal from '../../utils/hooks/useModal';
 
 export default function SignupSuccess() {
     const userType=useRecoilValue<string>(UserType)
     const [visible, setVisible]=useState<boolean>(false)
     const navigate=useNavigate()
+    const { modalRef } = useModal();
 
     function moveToHome(){
         navigate('/')
@@ -29,6 +31,17 @@ export default function SignupSuccess() {
     function moveToProducerUpload(page:string){
         navigate(page)
     }
+
+    const modalCloseHandler = (e:any) => {
+        if(visible && modalRef.current && !modalRef.current.contains(e.target)) setVisible(false);
+      };
+      
+      useEffect(() => {
+        window.addEventListener('click', modalCloseHandler);
+        return () => {
+          window.removeEventListener('click', modalCloseHandler);
+        };
+      });
 
   return (
     <SuccessPageContainer>
@@ -54,7 +67,7 @@ export default function SignupSuccess() {
             </UploadButtonWrapper>
         )}
         {visible&&(
-        <ModalWrapper>
+        <ModalWrapper ref={modalRef}>
             <MoveTouploadVocalSearchingButtonIcon onClick={()=>moveToProducerUpload(`/upload/${profileCategory.VOCAL_SEARCHING}`)}/>
             <MoveTouploadPortfolioButtonIcon onClick={()=>moveToProducerUpload(`/upload/${profileCategory.PORTFOLIO}`)}/>
         </ModalWrapper>
