12b181d3d32f45010ecb12366f838370bde4c588
myeongheonhong <ruddyh7076@gmail.com>
2023-03-17 09:47:22 +0900

    [ fix ] 서버데이터 변경으로 wavFile -> audioFile로 변경 및 에러케이스 수정

--- src/type/uploadInfoDataType.ts
+++ src/type/uploadInfoDataType.ts
@@ -1,7 +1,7 @@
 export interface UploadInfoDataType {
   title: string;
   category: string;
-  wavFile: File | null;
+  audioFile: File | null;
   introduce: string | undefined;
   keyword: Array<string>;
   jacketImage: File | Blob | FormData;
