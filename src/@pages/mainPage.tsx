2940f5dbbf0f2bc6eaead114b7fe8b2b60717b51
myeongheonhong <ruddyh7076@gmail.com>
2023-03-28 22:16:13 +0900

    [ etc ] 테스트 코드

--- src/@pages/mainPage.tsx
+++ src/@pages/mainPage.tsx
@@ -26,6 +26,8 @@ export default function MainPage() {
   const showModal = useRecoilValue(openConventionModal);
   const [tracksOrVocals, setTracksOrVocals] = useRecoilState(tracksOrVocalsCheck);
 
+  console.log("hello");
+
   function setVocalsImg(e: React.MouseEvent<HTMLDivElement>) {
     setBackground(hoverVocalsImg);
     setIsVocalsHover(true);
