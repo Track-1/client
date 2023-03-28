b9283e08e9eea4d97a854f58e99efb9122340796
Seojisoo20191941 <amy0480@naver.com>
2023-03-28 15:02:52 +0900

    [ design ] 타이틀 아이콘 반응형

--- src/@components/@common/portfoliosInform.tsx
+++ src/@components/@common/portfoliosInform.tsx
@@ -109,9 +109,9 @@ export default function PortfoliosInform(props: PortfolioPropsType) {
               {checkIsVocalSearching() && isHoveredNClicked() && (
                 <PortfolioViewMoreButton onClick={() => moveTrackPost(id)} />
               )}
-              {isTracksPage(tracksOrVocals) && checkIsPortfolio() && checkIsTitle() && <ProducerPortfolioTitleTextIc />}
-              {isVocalsPage(tracksOrVocals) && !checkIsPortfolio() && checkIsTitle() && <VocalPortfolioTitleTextIc />}
-              {!(checkIsTitle() && checkIsVocalSearching()) && <BlankIc />}
+              {isTracksPage(tracksOrVocals) && checkIsPortfolio() && checkIsTitle() && <ProducerPortfolioTitleTextIcon />}
+              {isVocalsPage(tracksOrVocals) && !checkIsPortfolio() && checkIsTitle() && <VocalPortfolioTitleTextIcon />}
+              {!(checkIsTitle() && checkIsVocalSearching()) && <BlankIcon />}
               {checkisEllipsis() && <EllipsisIcon onClick={clickEllipsis} />}
               {openEllipsisModal && checkisEllipsis() && (
                 <PortfolioUpdateModal
@@ -236,3 +236,15 @@ const InformTagWrapper = styled.div`
 const EllipsisIcon = styled(EllipsisIc)`
   cursor: pointer;
 `;
+
+const ProducerPortfolioTitleTextIcon=styled(ProducerPortfolioTitleTextIc)`
+  width: 13.4rem;
+`
+
+const VocalPortfolioTitleTextIcon=styled(VocalPortfolioTitleTextIc)`
+  width: 13.4rem;
+`
+
+const BlankIcon=styled(BlankIc)`
+  width: 13.4rem;
+`
