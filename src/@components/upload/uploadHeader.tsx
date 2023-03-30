361f420efd801c5c4ee7ec2cc7e057ca12889f3f
Seojisoo20191941 <amy0480@naver.com>
2023-03-31 02:07:47 +0900

    [ design ] 커서 포인터 추가

--- src/@components/upload/uploadHeader.tsx
+++ src/@components/upload/uploadHeader.tsx
@@ -26,6 +26,7 @@ export default function UploadHeader(props: PropsType) {
   const [openModal, setOpenModal] = useRecoilState<boolean>(uploadButtonClickedInTrackList);
   const [isUploadActive, setIsUploadActive] = useState<boolean>(false);
 
+  console.log("loginUserId"+loginUserId)
   const { mutate } = useMutation(() => UploadInfo(uploadData, userType, producerUploadType), {
     onSuccess: () => {
       // alert("업로드 성공");
