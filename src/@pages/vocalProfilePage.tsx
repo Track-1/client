a4824dcdd655b024e5eb823506cce81145b35dae
NaveOWO <pianowell96@gmail.com>
2023-04-01 02:58:49 +0900

    [ fix ] patch api 오류 수정

--- src/@pages/vocalProfilePage.tsx
+++ src/@pages/vocalProfilePage.tsx
@@ -59,6 +59,7 @@ export default function VocalProfilePage() {
 
   async function getData(page: number) {
     if (hasNextPage !== false) {
+      console.log(state, "9099090999090990");
       const response = await getVocalProfile(state, page);
       setIsMe(response?.isMe);
       setProfileData(response?.vocalProfile);
