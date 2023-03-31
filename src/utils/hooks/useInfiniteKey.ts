abcfd9305f7c0de732887b255097be70339d27bd
[talkingOrange] <sonrisa-bonita@naver.com>
2023-04-01 00:07:37 +0900

    [ feat ] key 값 처음부터 random 으로 뿌리기

--- src/utils/hooks/useInfiniteKey.ts
+++ src/utils/hooks/useInfiniteKey.ts
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
 export default function useInfiniteKey() {
-  const [key, setKey] = useState("");
-  //const excuteGetData = () => setKey(Math.random().toString(36));
+  const [key, setKey] = useState(Math.random().toString(36));
 
   function excuteGetData() {
     setKey(Math.random().toString(36));
   }
 
-  console.log(key);
   return { key, excuteGetData };
 }
