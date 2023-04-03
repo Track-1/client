d1293753b7423606b465781a34956b7b4de335d0
NaveOWO <pianowell96@gmail.com>
2023-04-03 16:40:57 +0900

    [ fix ] 프로필 & 포트폴리오 수정 오류 해결

--- src/@components/producerProfile/producerInfos.tsx
+++ src/@components/producerProfile/producerInfos.tsx
@@ -5,7 +5,7 @@ import BackButton from "../@common/backButton";
 import HashTag from "../trackPost/hashTag";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { tracksOrVocalsCheck } from "../../recoil/tracksOrVocalsCheck";
-import { Categories } from "../../core/constants/categories";
+import { Categories, CategoryId, CategoryText } from "../../core/constants/categories";
 import { useNavigate } from "react-router-dom";
 import { isTracksPage, isVocalsPage } from "../../utils/common/pageCategory";
 import { showPlayerBar } from "../../recoil/player";
@@ -16,10 +16,11 @@ interface PropsType {
   whom: string;
   whoamI: string;
   pauseAudio: any;
+  changeKey: () => void;
 }
 
 export default function ProducerInfos(props: PropsType) {
-  const { profileData, isMe, whom, pauseAudio, whoamI } = props;
+  const { profileData, isMe, whom, pauseAudio, whoamI, changeKey } = props;
   const navigate = useNavigate();
   const tracksOrVocals = useRecoilValue<string>(tracksOrVocalsCheck);
   const [showPlayer, setShowPlayer] = useRecoilState<boolean>(showPlayerBar);
@@ -32,7 +33,9 @@ export default function ProducerInfos(props: PropsType) {
           state: profileData,
         })
       : navigate(`/profile-edit/producer/${profileData.id}`, {
-          state: profileData,
+          state: {
+            profileData: profileData,
+          },
         });
   }
 
@@ -65,8 +68,12 @@ export default function ProducerInfos(props: PropsType) {
         <CategoryBox isSelected={true}>
           <CategoryIcon />
           <CategoryArray>
-            {Object.values(Categories).map((value) =>
-              profileData.category.includes(value) ? <Category>{value}</Category> : <NotCategory>{value}</NotCategory>,
+            {Object.keys(CategoryId).map((category) =>
+              profileData.category.includes(CategoryText[category]) ? (
+                <Category>{category}</Category>
+              ) : (
+                <NotCategory>{category}</NotCategory>
+              ),
             )}
           </CategoryArray>
         </CategoryBox>
