96f5a45ee7a50148643db3a7553d8ad2c6136d65
Seojisoo20191941 <amy0480@naver.com>
2023-04-07 01:32:04 +0900

    [ etc ] 오타 수정

--- src/utils/common/privateRoute.tsx
+++ src/utils/common/privateRoute.tsx
@@ -7,7 +7,7 @@ import { isLogin } from './isLogined';
 
 interface PrivateRouteProps {
   children ?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Componet Element
-  authentication : boolean; // true :인증을 반드시 해야하만 접속가능, false : 인증을 반디스 안해야만 접속 가능
+  authentication : boolean; // true :인증을 반드시 해야하만 접속가능, false : 인증을 반드시 안해야만 접속 가능
 }
 
 
