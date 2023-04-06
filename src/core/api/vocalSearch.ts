c8ebaa6d17adf8a65809f56b09f624e5b5489694
NaveOWO <pianowell96@gmail.com>
2023-04-07 04:20:36 +0900

    [ fix ] 보컬 필터링 오류 해결

--- src/core/api/vocalSearch.ts
+++ src/core/api/vocalSearch.ts
@@ -4,7 +4,7 @@ import { getCookie } from "../../utils/cookie";
 export async function getVocalsData(filteredUrlApi: string, isSelected: boolean, page: number) {
   try {
     const data = await axios.get(
-      `${process.env.REACT_APP_BASE_URL}/vocals/filter?${filteredUrlApi}&isSelected=${isSelected}&page=${page}&limit=3`,
+      `${process.env.REACT_APP_BASE_URL}/vocals/filter?${filteredUrlApi}&isSelected=${false}&page=${page}&limit=3`,
       {
         headers: {
           Authorization: `Bearer ${getCookie("accessToken")}`,
