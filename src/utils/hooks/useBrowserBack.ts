4b6c352dcf3ad4d46eb6a0305c7be21e96c966de
Seojisoo20191941 <amy0480@naver.com>
2023-04-08 03:45:02 +0900

    [ feat ] 브라우저 뒤로가기 클릭 시 플레이어 멈추는 훅 수정

--- src/utils/hooks/useBrowserBack.ts
+++ src/utils/hooks/useBrowserBack.ts
@@ -6,9 +6,8 @@ export default function useBrowserBack() {
 
     function pauseAudioWhenClickBack(){
         window.onpageshow = function(event) {
-            if ( event.persisted || (window.performance && window.performance.navigation.type == 2)) {
+            if ( event.persisted || (window.performance && window.performance.navigation.type === 2)) {
                 // Back Forward Cache로 브라우저가 로딩될 경우 혹은 브라우저 뒤로가기 했을 경우
-                // 이벤트 추가하는 곳
                 pausesPlayerAudio();
                 closePlayer();
             }
