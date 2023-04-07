31df808c49abc976dca5b7c6a84e97c928f12b82
Seojisoo20191941 <amy0480@naver.com>
2023-04-08 03:44:43 +0900

    [ feat ] 브라우저 뒤로가기 클릭 시 플레이어 멈추는 훅 추가

---
+++ src/utils/hooks/useBrowserBack.ts
@@ -0,0 +1,19 @@
+import React from 'react'
+import usePlayer from "./usePlayer";
+
+export default function useBrowserBack() {
+    const { pausesPlayerAudio, closePlayer } = usePlayer();
+
+    function pauseAudioWhenClickBack(){
+        window.onpageshow = function(event) {
+            if ( event.persisted || (window.performance && window.performance.navigation.type == 2)) {
+                // Back Forward Cache로 브라우저가 로딩될 경우 혹은 브라우저 뒤로가기 했을 경우
+                // 이벤트 추가하는 곳
+                pausesPlayerAudio();
+                closePlayer();
+            }
+        }
+    } 
+
+    return {pauseAudioWhenClickBack}
+}
