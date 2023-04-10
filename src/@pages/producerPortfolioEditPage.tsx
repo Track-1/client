71396fa5abe00d4616a8e50f6aceceb35e172785
[talkingOrange] <sonrisa-bonita@naver.com>
2023-04-11 00:32:38 +0900

    [ etc ] 불필요한 코드 제거

--- src/@pages/producerPortfolioEditPage.tsx
+++ src/@pages/producerPortfolioEditPage.tsx
@@ -191,7 +191,6 @@ export default function ProducerPortfolioEditPage() {
     isImageHovered ? setIsImageHovered(false) : setIsImageHovered(true);
   }
 
-  console.log(textareaHeight);
 
   return (
     <>
