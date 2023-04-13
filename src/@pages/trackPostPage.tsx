f4ab31a04baef7c36d4b837ff61b219af47b6858
Seojisoo20191941 <amy0480@naver.com>
2023-04-13 13:24:12 +0900

    [ design ] 제목 길면 다음줄로 넘어가도록

--- src/@pages/trackPostPage.tsx
+++ src/@pages/trackPostPage.tsx
@@ -263,7 +263,9 @@ export default function TrackPostPage() {
               <BackButtonWrapper onClick={movePreviousPage}>
                 <BackButton pauseAudio={pauseAudio} />
               </BackButtonWrapper>
+              <AudioTitleWrapper>
               <AudioTitle>{trackInfoData?.title}</AudioTitle>
+              </AudioTitleWrapper>
               <ProducerBox>
                 <ProfileImgWrapper>
                   <ProducerProfile src={trackInfoData?.producerProfileImage} alt="프로듀서 프로필 이미지" />
@@ -360,8 +362,16 @@ const BackButtonWrapper = styled.div`
   align-items: center;
 `;
 
+const AudioTitleWrapper=styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  
+  width: 47rem;
+`
+
 const AudioTitle = styled.h1`
   width: 47rem;
+  overflow-wrap: break-word;
 
   ${({ theme }) => theme.fonts.title}
 
