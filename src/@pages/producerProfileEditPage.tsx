0bd91f48e3ffe4afbc24f9bce5d977233dfb74eb
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-03 23:03:08 +0900

    [ feat ] pages 파일에 합치기

--- src/@pages/producerProfileEditPage.tsx
+++ src/@pages/producerProfileEditPage.tsx
@@ -1,3 +1,33 @@
+import styled from "styled-components";
+
+import EditLeftContainer from "../@components/producerPortfolioEdit/editLeftContainer/editLeftContainer";
+import EditRightontainer from "../@components/producerPortfolioEdit/editRightContainer/editRightContainer";
+
 export default function ProducerProfileEditPage() {
-  return <>프로듀서프로필수정</>;
+  return (
+    <>
+      프로듀서프로필수정
+      <Temporary>
+        <TemporaryLeft>
+          <EditLeftContainer />
+        </TemporaryLeft>
+        <TemporaryRight>
+          <EditRightontainer />
+        </TemporaryRight>
+      </Temporary>
+    </>
+  );
 }
+
+const Temporary = styled.div`
+  color: white;
+  font-size: 2rem;
+`;
+
+const TemporaryLeft = styled.div`
+  float: left;
+`;
+
+const TemporaryRight = styled.div`
+  float: right;
+`;
