9a58036df81231e687fbc5d11da7216e7025cc36
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-04 22:53:58 +0900

    [ feat ] 왼쪽 오른쪽 컴포넌트 삭제

--- src/@components/producerProfileEdit/editLeftContainer/editLeftContainer.tsx
+++
@@ -1,9 +0,0 @@
-import ProfileImage from "./profileImage";
-import ProfileName from "./profileName";
-
-export default function EditLeftContainer() {
-  return <>왼쪽 div
-  <ProfileImage/>
-  <ProfileName/>
-  </>;
-}
