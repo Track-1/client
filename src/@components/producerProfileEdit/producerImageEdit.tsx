a57213c58aff9ccd7e5bcd7466f7fda6fb120b53
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-05 23:03:31 +0900

    [ refactor ] 프로듀서 프로필 이미지 업로더 퍼블 리팩토링

--- src/@components/producerProfileEdit/producerImageEdit.tsx
+++ src/@components/producerProfileEdit/producerImageEdit.tsx
@@ -1,4 +1,51 @@
+import styled from "styled-components";
+
+import { ChangePhotoIc } from "../../assets";
+
 export default function ProducerImageEdit() {
-    return <>프로듀서 프로필 이미지 업로더</>;
+  return (
+    <>
+      프로듀서 프로필 이미지 업로더
+      <ProfileImageContainer htmlFor="profileImg">
+        {/* 사용자가 넣은 이미지 or 기본 사람 이미지 */}
+        {/* {isImageUploaded ? <ProfileImage src={String(showImage)} /> : <ProfileImage src={String(profileImage)} />} */}
+        <ChangePhotoIcon />
+      </ProfileImageContainer>
+    </>
+  );
+}
+
+const ChangePhotoIcon = styled(ChangePhotoIc)`
+  width: 36.8rem;
+  height: 36.8rem;
+
+  display: none;
+  position: absolute;
+
+  border: 0.1rem solid rgba(30, 32, 37, 0.5);
+  border-radius: 25rem;
+  backdrop-filter: blur(1.7rem);
+
+  cursor: pointer;
+`;
+
+const ProfileImageContainer = styled.label`
+  width: 36.8rem;
+  height: 36.8rem;
+
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+
+  overflow: hidden;
+  border-radius: 50%;
+
+  margin-top: 17.8rem;
+
+  :hover {
+    ${ChangePhotoIcon} {
+      display: block;
+    }
   }
-  
+`;
