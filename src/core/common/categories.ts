2e80a68e3affc1d98595840172c3c9b2188289f8
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-08 21:46:43 +0900

    [ feat ] 카테고리 상수값 및 객체 타입 파일 추가

---
+++ src/core/common/categories.ts
@@ -0,0 +1,51 @@
+import { CategoryIdType, CategorySelectType } from "../../type/common/CategoryChecksType";
+
+export const Categories: string[] = ["R&B", "Hiphop", "Ballad", "Pop", "Rock", "EDM", "JAZZ", "House", "Funk"];
+
+export const CategoryId: CategoryIdType = {
+  "R&B": "0",
+  HIPHOP: "1",
+  BALLAD: "2",
+  POP: "3",
+  ROCK: "4",
+  EDM: "5",
+  JAZZ: "6",
+  HOUSE: "7",
+  FUNK: "8",
+};
+
+export const CategoryBoolean: CategorySelectType = {
+  "R&B": false,
+  HIPHOP: false,
+  BALLAD: false,
+  POP: false,
+  ROCK: false,
+  EDM: false,
+  JAZZ: false,
+  HOUSE: false,
+  FUNK: false,
+};
+
+export const CategoryDropdown: CategoryIdType = {
+  "r&b": "R&B",
+  hiphop: "Hiphop",
+  ballad: "Ballad",
+  pop: "Pop",
+  rock: "Rock",
+  edm: "EDM",
+  jazz: "JAZZ",
+  house: "House",
+  funk: "Funk",
+};
+
+export const CategoryText: CategoryIdType = {
+  "R&B": "R&B",
+  HIPHOP: "Hiphop",
+  BALLAD: "Ballad",
+  POP: "Pop",
+  ROCK: "Rock",
+  EDM: "EDM",
+  JAZZ: "JAZZ",
+  HOUSE: "House",
+  FUNK: "Funk",
+};
---
+++ src/type/common/CategoryChecksType.ts
@@ -0,0 +1,14 @@
+export interface CategoryChecksType {
+  categId: number;
+  selected: boolean;
+}
+
+export interface CategoryIdType {
+  [key: string]: string;
+}
+
+export interface CategorySelectType {
+  [key: string]: boolean;
+}
+
+export type CategoriesDropdownType = "R&B" | "Hiphop" | "Ballad" | "Pop" | "Rock" | "EDM" | "JAZZ" | "House" | "Funk";
