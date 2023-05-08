b330b886ed1af14213e2ef416abca1f5103ec09e
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-08 21:55:17 +0900

    [ feat ] titleInput UI

--- src/@components/@common/titleInputEdit.tsx
+++ src/@components/@common/titleInputEdit.tsx
@@ -1,4 +1,101 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+enum nickName {
+  NOTHING = "nothing",
+  CORRECT = "correct",
+  ERROR = "error",
+}
+
 export default function TitleInputEdit() {
-    return <> 소제목 + input창</>;
-  }
-  
+  const [nameState, setNameState] = useState<nickName>(nickName.NOTHING);
+
+  return (
+    <>
+      {" "}
+      소제목 + input창
+      <NameContainer>
+        <NameTitleWrapper>
+          <NameTitleText>Name</NameTitleText>
+          <PointIcon />
+        </NameTitleWrapper>
+        <InputWrapper nameState={nameState}></InputWrapper>
+        {nameState === nickName.ERROR ? (
+          <ProfileEditWarningMsg>
+            1 to 16 characters(Korean, English), numbers or special characters.
+          </ProfileEditWarningMsg>
+        ) : (
+          <BlankMessage></BlankMessage>
+        )}
+      </NameContainer>
+    </>
+  );
+}
+
+const NameContainer = styled.article`
+  height: 8.8rem;
+  width: 60rem;
+
+  margin: 7.6rem 0 0 6.4rem;
+`;
+
+const NameTitleWrapper = styled.div`
+  display: flex;
+`;
+
+const NameTitleText = styled.strong`
+  color: ${({ theme }) => theme.colors.gray2};
+  ${({ theme }) => theme.fonts.cations}
+
+  margin-right: 0.6rem;
+`;
+
+const PointIcon = styled.div`
+  height: 0.7rem;
+  width: 0.7rem;
+
+  background-color: ${({ theme }) => theme.colors.main};
+`;
+
+const InputWrapper = styled.div<{ nameState: nickName }>`
+  width: 54.9rem;
+
+  display: flex;
+  justify-content: space-between;
+
+  margin-bottom: 0.5rem;
+
+  border-bottom: ${({ nameState }) => {
+    if (nameState === "nothing") return "0.1rem solid white";
+    if (nameState === "correct") return "0.1rem solid #5200FF";
+    if (nameState === "error") return "0.1rem solid  #FF4F4F";
+  }};
+`;
+
+const ProfileEditWarningMsg = styled.span`
+  width: 100%;
+  height: 3rem;
+  ${({ theme }) => theme.fonts.description};
+  color: ${({ theme }) => theme.colors.red};
+
+  margin-top: 1.1rem;
+`;
+
+const NameInput = styled.input`
+  height: 4.5rem;
+  width: 70%;
+
+  margin-top: 1rem;
+
+  color: ${({ theme }) => theme.colors.white};
+
+  ${({ theme }) => theme.fonts.input}
+`;
+
+const BlankMessage = styled.p`
+  width: 100%;
+  height: 3rem;
+
+  color: transparent;
+  margin-bottom: 2rem;
+`;
