d6bb3cd4c462a75e87adbb1d5aab1b4ca41db417
myeongheonhong <ruddyh7076@gmail.com>
2023-05-10 01:41:37 +0900

    [ style ] 업로드 헤더 컴포넌트 UI구현

---
+++ src/@components/upload/uploadHeader.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import uploadAbleBtnImg from "../../assets/image/uploadAbleBtnImg.png";
+import uploadUnableBtnImg from "../../assets/image/uploadUnableBtnImg.png";
+
+interface UploadHeaderProps {
+  // userType: string;
+  isUploadActive: boolean;
+}
+export default function UploadHeader(props: UploadHeaderProps) {
+  const { isUploadActive } = props;
+  return (
+    <Container>
+      {/* back버튼 */}
+      <UploadTypeText>Back</UploadTypeText>
+      <Wrapper>
+        <UploadTypeText>Vocal Searching</UploadTypeText>
+        {isUploadActive ? (
+          <img src={uploadAbleBtnImg} alt="업로드 버튼" />
+        ) : (
+          <img src={uploadUnableBtnImg} alt="업로드 버튼" />
+        )}
+      </Wrapper>
+    </Container>
+  );
+}
+
+const Container = styled.header`
+  display: flex;
+  align-items: center;
+
+  height: 17rem;
+
+  padding: 0 7.5rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+`;
+
+const UploadTypeText = styled.div`
+  color: ${({ theme }) => theme.colors.gray3};
+  ${({ theme }) => theme.fonts.id};
+`;
---
+++ src/@pages/uploadPage.tsx
@@ -0,0 +1,9 @@
+import UploadHeader from "../@components/upload/uploadHeader";
+
+export default function UploadPage() {
+  return (
+    <>
+      <UploadHeader isUploadActive={true} />
+    </>
+  );
+}
