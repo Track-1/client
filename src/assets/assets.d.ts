26a5014f3203e291c75c6c54e6b3f4bbb3d98353
myeongheonhong <ruddyh7076@gmail.com>
2023-05-10 01:40:48 +0900

    [ style ] 이미지 추가

---
+++ src/assets/assets.d.ts
@@ -0,0 +1,17 @@
+declare module "*.mp3";
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "*.wav" {
+  const content: string;
+  export default content;
+}
