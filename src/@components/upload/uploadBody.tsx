a4126ae63fa96d2af6a13ec67cb65431e5a0fa5e
myeongheonhong <ruddyh7076@gmail.com>
2023-05-11 02:28:04 +0900

    [ feat ] uploadBody / ProducerLayout 컴포넌트 UI구현

---
+++ src/@components/upload/uploadBody.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import ProducerLayout from "./producerLayout";
+
+export default function UploadBody() {
+  return (
+    <Container>
+      <ProducerLayout>
+        <></>
+      </ProducerLayout>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
--- src/@pages/uploadPage.tsx
+++ src/@pages/uploadPage.tsx
@@ -1,9 +1,11 @@
+import UploadBody from "../@components/upload/uploadBody";
 import UploadHeader from "../@components/upload/uploadHeader";
 
 export default function UploadPage() {
   return (
     <>
       <UploadHeader isUploadActive={true} />
+      <UploadBody />
     </>
   );
 }
