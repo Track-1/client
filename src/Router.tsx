be782207d8a47f022c6ed0a26c5a60125f2bfce5
Seojisoo20191941 <amy0480@naver.com>
2023-05-13 00:52:11 +0900

    [ feat ] 로그인 성공페이지 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupSuccessPage from "./@pages/signupSuccessPage";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/signup/success" element={<SignupSuccessPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
