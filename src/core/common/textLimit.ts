70557f20e480d32e2bbab655e3b965bc979a07cb
myeongheonhong <ruddyh7076@gmail.com>
2023-05-13 02:12:00 +0900

    [ feat ] 입력제한 숫자 상수로 구현

---
+++ src/core/common/textLimit.ts
@@ -0,0 +1,11 @@
+import { TextLimitType } from "../../type/common/textLimitType";
+
+export const TEXT_LIMIT: TextLimitType = {
+  none: -1,
+  10: 10,
+  15: 15,
+  20: 20,
+  36: 36,
+  150: 150,
+  200: 200,
+};
---
+++ src/type/common/textLimitType.ts
@@ -0,0 +1,9 @@
+export interface TextLimitType {
+  "none": number;
+  "10": number;
+  "15": number;
+  "20": number;
+  "36": number;
+  "150": number;
+  "200": number;
+}
