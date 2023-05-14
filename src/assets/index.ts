7ee0de8f9c726dad5c64f25d3c2f90a40cc7dbbe
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-14 22:10:49 +0900

    [ etc ] 불필요한 아이콘 삭제

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,5 +4,4 @@ export { ReactComponent as AddHashtagIc } from "./icon/addHashtagIc.svg";
 export { ReactComponent as DeleteHashtagIc } from "./icon/deleteHashtagIc.svg";
 export { ReactComponent as HashtagTitleIc } from "./icon/hashtagTitleIc.svg";
 export { ReactComponent as HashtagWarningIc } from "./icon/hashtagWarningIc.svg";
-export { ReactComponent as HoverHashtagWarningIc } from "./icon/hoverHashtagWarningIc.svg";
 export { ReactComponent as ProfileBackgroundIc } from "./icon/profileBackgroundIc.svg";
