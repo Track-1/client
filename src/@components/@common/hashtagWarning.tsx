e0fd91f7926efb400de63bc0bb94b3b5e0a6bc53
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-14 22:11:42 +0900

    [ feat ] useHover 삭제, style-components로 구현

--- src/@components/@common/hashtagWarning.tsx
+++ src/@components/@common/hashtagWarning.tsx
@@ -1,32 +1,19 @@
-import React from "react";
 import styled from "styled-components";
-import { HashtagWarningIc, HoverHashtagWarningIc } from "../../assets";
-import useHover from "../../hooks/common/useHover";
+import { HashtagWarningIc } from "../../assets";
 
 export default function ProfileWarning() {
-  const { hoverState, changeHoverState } = useHover();
-
   return (
     <WarningIcon>
-      {hoverState ? (
-        <>
-          <HoverHashtagWarningIcon
-            onMouseEnter={(e) => changeHoverState(e)}
-            onMouseLeave={(e) => changeHoverState(e)}
-          />
-          <WarningTextWrapper>
-            <WarningText>
-              1. 해시태그는 최대 3개까지 추가 가능합니다.
-              <br />
-              2. 최대 10자까지 작성이 가능합니다.
-              <br />
-              3. 트랙의 분위기에 대해 설명해주세요. (ex. tropical, dynamic)
-            </WarningText>
-          </WarningTextWrapper>
-        </>
-      ) : (
-        <HashtagWarningIcon onMouseEnter={(e) => changeHoverState(e)} onMouseLeave={(e) => changeHoverState(e)} />
-      )}
+      <HashtagWarningIcon />
+      <WarningTextWrapper>
+        <WarningText>
+          1. 해시태그는 최대 3개까지 추가 가능합니다.
+          <br />
+          2. 최대 10자까지 작성이 가능합니다.
+          <br />
+          3. 트랙의 분위기에 대해 설명해주세요. (ex. tropical, dynamic)
+        </WarningText>
+      </WarningTextWrapper>
     </WarningIcon>
   );
 }
@@ -41,6 +28,8 @@ const WarningTextWrapper = styled.div`
   background: rgba(30, 32, 37, 0.7);
   backdrop-filter: blur(0.3rem);
   border-radius: 0.5rem;
+
+  display: none;
 `;
 
 const WarningText = styled.div`
@@ -54,26 +43,24 @@ const WarningIcon = styled.div`
   height: 3rem;
   margin-top: 0.7rem;
   border-radius: 5rem;
+  position: relative;
 
   cursor: pointer;
+
+  :hover ${WarningTextWrapper} {
+    display: block;
+  }
 `;
 
-const HoverHashtagWarningIcon = styled(HoverHashtagWarningIc)`
+const HashtagWarningIcon = styled(HashtagWarningIc)`
   position: absolute;
   z-index: 1;
   width: 4rem;
   height: 4rem;
   margin-left: 42.6rem;
   margin-top: -1rem;
-`;
 
-const HashtagWarningIcon = styled(HashtagWarningIc)`
-  width: 4rem;
-  height: 4rem;
-  margin-left: 42.6rem;
-  margin-top: -1rem;
-`;
-
-const HashIconWrapper = styled.div`
-  display: flex;
+  :hover {
+    opacity: 0.3;
+  }
 `;
