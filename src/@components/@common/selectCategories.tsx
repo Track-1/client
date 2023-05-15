5c6230dff78c9578252184b7c0ebb55f5c1efadc
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-16 00:15:38 +0900

    [ etc ] 컴포넌트 첫 글자 대문자로 수정

--- src/@components/@common/selectCategories.tsx
+++ src/@components/@common/selectCategories.tsx
@@ -6,7 +6,7 @@ import { CategorySelectType } from "../../type/common/CategoryChecksType";
 import { CategoryTitleIc } from "../../assets";
 import styled from "styled-components";
 
-export default function selectCategory() {
+export default function SelectCategory() {
   const [isCategorySelected, setIsCategorySelected] = useState<CategorySelectType>(CategoryBoolean);
 
   function handleSelectCategory(category: string) {
