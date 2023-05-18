ae14964dcde8c2885273246055ea018a54819027
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-18 23:34:16 +0900

    [ feat ] input 파일 입력시 onChange로 파일 업데이트

--- src/@components/producerProfileEdit/producerImageEdit.tsx
+++ src/@components/producerProfileEdit/producerImageEdit.tsx
@@ -3,14 +3,19 @@ import styled from "styled-components";
 import { ChangePhotoIc } from "../../assets";
 
 export default function ProducerImageEdit() {
+  function getImageFile(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.files);
+  }
+
   return (
     <>
       {/* 프로듀서 프로필 이미지 업로더 */}
       <ProfileImageContainer>
         {/* 사용자가 넣은 이미지 or 기본 사람 이미지 */}
         {/* {isImageUploaded ? <ProfileImage src={String(showImage)} /> : <ProfileImage src={String(profileImage)} />} */}
+        <ProfileImage />
         <ChangePhotoIcon />
-        <FileInput type="file" />
+        <FileInput type="file" onChange={getImageFile} />{" "}
       </ProfileImageContainer>
     </>
   );
@@ -46,6 +51,16 @@ const ProfileImageContainer = styled.label`
   :hover ${ChangePhotoIcon} {
     display: block;
   }
+`;
+
+const ProfileImage = styled.img`
+  width: 100%;
+  height: 100%;
+
+  margin: auto;
+
+  transform: translate(50, 50);
+  object-fit: cover;
 `;
 
 const FileInput = styled.input`
