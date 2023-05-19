2f79e0e979d29aae2178dde938fd4c6b822de13c
myeongheonhong <ruddyh7076@gmail.com>
2023-05-20 00:32:52 +0900

    [ etc ] 이미지 파일 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,6 @@
 export { ReactComponent as UploadFileChangeIc } from "./icon/uploadFileChangeIc.svg";
+export { ReactComponent as UploadCategoryIc } from "./icon/uploadCategoryIc.svg";
+export { ReactComponent as UploadFileIc } from "./icon/uploadFileIc.svg";
+export { ReactComponent as UploadDescriptionIc } from "./icon/uploadDescriptionIc.svg";
+export { ReactComponent as FolderUploadIc } from "./icon/folderUploadIc.svg";
+export { ReactComponent as CategoryDropDownIc } from "./icon/categoryDropDownIc.svg";
