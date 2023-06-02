c432759f64f9c83f1e273aea3454f5cda0c73830
Seojisoo20191941 <amy0480@naver.com>
2023-06-03 03:46:37 +0900

    [ feat ] 이메일 형식 검사 함수 구현

---
+++ src/utils/signUp/checkForm.ts
@@ -0,0 +1,4 @@
+export function checkEmailForm(email: string) {
+  const regex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  return regex.test(email);
+}
