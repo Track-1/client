3a2970bf008362ebaf6a92777e82cadce18460ff
[talkingOrange] <sonrisa-bonita@naver.com>
2023-06-05 22:16:22 +0900

    [ feat ] hashtag 동적 width 함수

--- src/@components/@common/hashtagsEdit.tsx
+++ src/@components/@common/hashtagsEdit.tsx
@@ -1,29 +1,37 @@
 import styled from "styled-components";
-import {
-  AddHashtagIc,
-  DeleteHashtagIc,
-  HashtagTitleIc,
-} from "../../assets";
+import { useState } from "react";
+
+import { AddHashtagIc, DeleteHashtagIc, HashtagTitleIc } from "../../assets";
 import HashtagWarning from "./hashtagWarning";
 
 export default function HashtagsEdit() {
+  const [isKorean, setIsKorean] = useState<boolean>(false);
+  const [hashtagLength, setHashtagLength] = useState<number>(0);
+
+  function checkHashtagText(e: React.ChangeEvent<HTMLInputElement>) {
+    let hashtagValue = e.target.value;
+    hashtagValue !== "" ? setHashtagLength(hashtagValue.length) : setHashtagLength(0);
+  }
+
   return (
     <>
-     
       <HashtagContainer>
         <HashIconWrapper>
           <HashtagTitleIcon />
           <HashtagWarning />
         </HashIconWrapper>
         <InputHashtagWrapper>
-
-        <Hashtag>
-          <HashtagWrapper> 
-            <HashtagSharp># </HashtagSharp>
-            <HashtagInput/>
+          <Hashtag>
+            <HashtagWrapper>
+              <HashtagSharp># </HashtagSharp>
+              <HashtagInput
+                placeholder="Hashtag"
+                onChange={checkHashtagText}
+                inputWidth={hashtagLength}
+              />
             </HashtagWrapper>
-        </Hashtag>
-        <AddHashtagIcon />
+          </Hashtag>
+          <AddHashtagIcon />
           {/* {hashtags?.map((hashtag, index) => {
             return (
               <Hashtag key={index}>
@@ -79,7 +87,7 @@ const HashtagTitleIcon = styled(HashtagTitleIc)`
 const InputHashtagWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  
+
   height: 10rem;
 
   align-items: center;
@@ -87,42 +95,42 @@ const InputHashtagWrapper = styled.div`
 
 const Hashtag = styled.div`
   display: flex;
-  
+
   height: 3.8rem;
-  
+
   padding-right: 1rem;
   margin-right: 1rem;
-  
+
   border-radius: 2.1rem;
   background-color: ${({ theme }) => theme.colors.gray5};
-  
+
   align-items: center;
 `;
 
 const HashtagWrapper = styled.div`
   display: flex;
-  
+
   padding: 0 0.5rem 0 1.5rem;
-  
+
   align-items: center;
 `;
 
 const HashtagSharp = styled.p`
   margin-right: 0.5rem;
-  
+
   color: ${({ theme }) => theme.colors.gray1};
   ${({ theme }) => theme.fonts.hashtag};
 `;
 
 const HashtagInput = styled.input<{ inputWidth: number; isKorean: boolean }>`
   display: flex;
-  
+
   width: ${({ inputWidth, isKorean }) =>
     inputWidth === 0 ? 9 : isKorean ? inputWidth * 1.5 + 1 : inputWidth * 1.2 + 1}rem;
   ${({ theme }) => theme.fonts.hashtag};
-  
+
   color: ${({ theme }) => theme.colors.gray1};
-  
+
   ::placeholder {
     color: ${({ theme }) => theme.colors.gray3};
   }
@@ -130,7 +138,7 @@ const HashtagInput = styled.input<{ inputWidth: number; isKorean: boolean }>`
 
 const CompletedHashtag = styled.article`
   display: flex;
-  
+
   color: ${({ theme }) => theme.colors.white};
   ${({ theme }) => theme.fonts.hashtag}
   align-items: center;
@@ -139,7 +147,7 @@ const CompletedHashtag = styled.article`
 const AddHashtagIcon = styled(AddHashtagIc)`
   width: 4rem;
   height: 4rem;
-  
+
   margin-top: 1rem;
 
   cursor: pointer;
@@ -149,14 +157,14 @@ const DeleteHashtagIcon = styled(DeleteHashtagIc)`
   width: 2.8rem;
 
   margin-left: -1rem;
-  
+
   cursor: pointer;
 `;
 
 const CompleteHashtagWrapper = styled.div`
   display: flex;
-  
+
   padding: 0 1.5rem;
-  
+
   align-items: center;
 `;
