800df5aa9f3388eee7998771a904396688952475
Seojisoo20191941 <amy0480@naver.com>
2023-06-08 03:00:13 +0900

    [ etc ] 불필요 코드 삭제

--- src/api/signup.ts
+++ src/api/signup.ts
@@ -9,25 +9,6 @@ export async function authEmail(formData: any) {
   return data;
 }
 
-// export async function checkEmailDuplication(formData: any) {
-//   const data = await axios.post(`${process.env.REACT_APP_BASE_URL}/user/etc/check-email`, formData, {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-
-//   return data.data.data.isDuplication;
-// }
-
-// export async function repostAuthEmail(formData: any) {
-//   const data = await axios.patch(`${process.env.REACT_APP_BASE_URL}/user/etc/auth-mail-repost`, formData, {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   return data;
-// }
-
 export async function postVerifyCode(formData: any) {
   const data = await axios.post(`${process.env.REACT_APP_BASE_URL}/user/etc/verify`, formData, {
     headers: {
