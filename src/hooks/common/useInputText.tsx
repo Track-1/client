ca6de69de88f4b3eb94787268051d5e033067366
myeongheonhong <ruddyh7076@gmail.com>
2023-06-24 12:17:14 +0900

    [ refactor ] 커스텀훅 인자 옵셔널로 변경

--- src/hooks/common/useInputText.tsx
+++ src/hooks/common/useInputText.tsx
@@ -1,11 +1,10 @@
 import { useState, useCallback } from "react";
 
-export default function useInputText(initValue: string, inputLimit: number) {
+export default function useInputText(initValue: string, inputLimit?: number) {
   const [value, setValue] = useState(initValue);
 
   const changeInput = useCallback((e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-    //-1일 경우 입력제한이 없는 Input
-    if (e.currentTarget.value.length <= inputLimit || inputLimit === -1) {
+    if (inputLimit && e.currentTarget.value.length <= inputLimit) {
       setValue(e.currentTarget.value);
     }
   }, []);
