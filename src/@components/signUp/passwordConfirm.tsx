fc6438f39d74436f9fdb5989e4f6601ff0947734
Seojisoo20191941 <amy0480@naver.com>
2023-08-02 16:16:11 +0900

    [ feat ] 비밀번호 확인  보이기 로직 추가

--- src/@components/signUp/passwordConfirm.tsx
+++ src/@components/signUp/passwordConfirm.tsx
@@ -1,19 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FormProvider } from "react-hook-form";
+import styled from "styled-components";
+import { ShowPasswordSignupIc, UnshowPasswordSignupIc } from "../../assets";
 import { PASSWORD_MESSAGE } from "../../core/signUp/errorMessage";
 import { SignupInputProps } from "../../type/signUp/inputProps";
+import { showPassword } from "../../utils/signUp/showPassword";
 import Input from "./Input";
 import InputTitle from "./inputTitle";
 
 export default function PasswordConfirm(props: SignupInputProps) {
   const { methods } = props;
-
   const {
     handleSubmit,
     setError,
     formState: { errors },
     watch,
   } = methods;
+  const [isShow, setIsShow] = useState(false);
 
   function checkPasswordMatch() {
     return watch("password") === watch("passwordConfirm");
@@ -25,6 +28,10 @@ export default function PasswordConfirm(props: SignupInputProps) {
     }
   }, [watch("passwordConfirm")]);
 
+  function handleShowPassword() {
+    setIsShow(!isShow);
+  }
+
   return (
     <>
       <FormProvider {...methods}>
@@ -35,12 +42,32 @@ export default function PasswordConfirm(props: SignupInputProps) {
             rules={{
               required: true,
             }}
-            type="password"
+            type={showPassword(isShow)}
             placeholder="Enter a password again"
             width={56}
           />
+          <EyeWrapper onClick={handleShowPassword}>
+            {isShow ? <ShowPasswordSignupIcon /> : <UnshowPasswordSignupIcon />}
+          </EyeWrapper>
         </form>
       </FormProvider>
     </>
   );
 }
+
+const EyeWrapper = styled.i`
+  margin: -7.5rem 0 0 52rem;
+  position: absolute;
+
+  cursor: pointer;
+`;
+
+const ShowPasswordSignupIcon = styled(ShowPasswordSignupIc)`
+  width: 4rem;
+  height: 4rem;
+`;
+
+const UnshowPasswordSignupIcon = styled(UnshowPasswordSignupIc)`
+  width: 4rem;
+  height: 4rem;
+`;
