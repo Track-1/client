7bc7a32f85910035bf55833ca246ff96b794a38f
Seojisoo20191941 <amy0480@naver.com>
2023-08-08 01:46:14 +0900

    [ feat ] success 페이지를 프로듀서와 보컬 따로 구현

--- src/@pages/signupSuccessPage.tsx
+++ src/@pages/signupSuccessPage.tsx
@@ -1,3 +1,16 @@
+import { useRecoilValue } from "recoil";
+import ProducerSuccess from "../@components/signUp/producerSuccess";
+import VocalSuccess from "../@components/signUp/vocalSuccess";
+import { role } from "../recoil/common/role";
+import { isProducer, isVocal } from "../utils/common/checkRoleType";
+
 export default function SignupSuccessPage() {
-  return <></>;
+  const userType = useRecoilValue(role);
+
+  return (
+    <>
+      {isProducer(userType) && <ProducerSuccess />}
+      {isVocal(userType) && <VocalSuccess />}
+    </>
+  );
 }
