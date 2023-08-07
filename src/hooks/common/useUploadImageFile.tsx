2744940fe75ef1b835a1ef9d1d3f32ea3872c507
myeongheonhong <ruddyh7076@gmail.com>
2023-08-07 13:10:50 +0900

    [ feat ] 이미지 타입, 사이즈 체크조건 변경

--- src/hooks/common/useUploadImageFile.tsx
+++ src/hooks/common/useUploadImageFile.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
-import { LIMIT_IMAGE_SIZE } from "../../core/common/fileSize";
+import { IMAGE_FILE_TYPE } from "../../core/common/fileType";
+import { uploadImageTypeWarningMessage } from "../../core/common/warningMessage";
+import { checkFileSize } from "../../utils/common/checkFileSize";
 
 export default function useUploadImageFile() {
   const [imageFile, setImageFile] = useState<File | Blob | null>(null);
   const [previewImage, setPreviewImage] = useState<string | ArrayBuffer | null>("");
 
-  //오디오 업로드
+  
   function uploadImageFile(e: React.ChangeEvent<HTMLInputElement>) {
     const imageFile = e.target.files && e.target.files[0];
-    const imageSize = imageFile?.size;
-    const imageType = getImageType(imageFile?.type);
+    const imageSize = imageFile && checkFileSize(imageFile.size) ? imageFile.size : 0;
+    const imageType = imageFile ? getImageType(imageFile.name) : "";
 
-    if (checkImageSize(imageSize) && checkImageType(imageType)) {
+    if (imageSize && imageType) {
       setImageFile(imageFile);
 
       const reader = new FileReader();
@@ -22,40 +24,14 @@ export default function useUploadImageFile() {
     }
   }
 
-  function getImageType(uploadName: string | undefined) {
-    if (uploadName) {
-      return uploadName.substring(uploadName.length - 4);
-    }
+  function getImageType(uploadName: string) {
+    const type = uploadName.substring(uploadName.length - 4);
+    console.log(type);
+    return checkImageType(type) ? type : alert(uploadImageTypeWarningMessage);
   }
 
-  function checkImageSize(imageSize: number | undefined) {
-    if (imageSize) {
-      if (imageSize > LIMIT_IMAGE_SIZE)
-        alert("Only files under 5 MB can be uploaded.\n5MB 이하의 파일만 업로드 가능합니다.");
-      return imageSize < LIMIT_IMAGE_SIZE;
-    }
-  }
-
-  function checkImageType(fileType: string | undefined) {
-    !(
-      fileType === ".jpg" ||
-      fileType === "jpeg" ||
-      fileType === ".png" ||
-      fileType === ".JPG" ||
-      fileType === ".JPEG" ||
-      fileType === ".PNG"
-    ) &&
-      alert(
-        "You can only upload images in jpg, jpeg, and png formats.\njpg, jpeg, png형식의 이미지만 업로드할 수 있습니다.",
-      );
-    return (
-      fileType === ".jpg" ||
-      fileType === "jpeg" ||
-      fileType === ".png" ||
-      fileType === ".JPG" ||
-      fileType === ".JPEG" ||
-      fileType === ".PNG"
-    );
+  function checkImageType(fileType: string) {
+    return Object.keys(IMAGE_FILE_TYPE).includes(fileType);
   }
 
   return { imageFile, previewImage, uploadImageFile };
