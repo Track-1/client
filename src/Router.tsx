d8f303e62ad4f527a317bbbfe7c27b9a2a9887a2
myeongheonhong <ruddyh7076@gmail.com>
2023-08-07 15:04:04 +0900

    [ etc ] 라우터 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UploadPage from "./@pages/uploadPage";
 import ProducerProfileEditPage from "./@pages/producerProfileEditPage";
 import VocalProfileEditPage from "./@pages/vocalProfileEditPage";
+import ProducerPortfolioEditPage from "./@pages/producerPortfolioEditPage";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/upload" element={<UploadPage />} />
-        <Route path="/portfolio-edit/producer/:portfolioId" element={<UploadPage />} />
+        <Route path="/portfolio-edit/producer/:portfolioId" element={<ProducerPortfolioEditPage />} />
         <Route path="/portfolio-edit/vocal/:portfolioId" element={<UploadPage />} />
         {/* [ 예원 ] 리팩토링 임시 라우터 */}
         <Route path="/producer/profile-edit" element={<ProducerProfileEditPage />} />
