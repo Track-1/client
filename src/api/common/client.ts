0f4ec6b63b721f428fadedf08c3ae7180513d24c
Seojisoo20191941 <amy0480@naver.com>
2023-08-08 16:26:25 +0900

    [ feat ] client 인스턴스 생성

---
+++ src/api/common/client.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { getCookie, removeCookie, setCookie } from "../../../utils/cookie";
+
+//서버통신 함수
+export const client = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${getCookie("accessToken")}`,
+  },
+  withCredentials: true,
+});
+
+client.interceptors.request.use(function (config: any) {
+  const token = getCookie("accessToken");
+
+  if (!token) {
+    config.headers["accessToken"] = null;
+    return config;
+  }
+
+  if (config.headers && token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+    return config;
+  }
+  return config;
+});
+
+client.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    console.log(error);
+
+    const originConfig = error.config;
+
+    if (error.response && error.response.status === 401) {
+      try {
+        const data = await axios.get(`${process.env.REACT_APP_BASE_URL}/user/etc/refresh`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getCookie("accessToken")}`,
+          },
+          withCredentials: true,
+        });
+        if (data) {
+          setCookie("accessToken", data.data.data, {});
+          return await client.request(originConfig);
+        }
+      } catch (error: any) {
+        if (error.response.data.message === "새롭게 로그인 필요") {
+          alert("Token expired, please log in again.\n토큰이 만료되어 다시 로그인 바랍니다.");
+          removeCookie("accessToken", { path: "/" });
+          window.location.replace("/login");
+        }
+      }
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  },
+);
