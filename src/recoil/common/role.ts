4e466bc5eef43b3b444618cd7c3535dc38f8d50f
Seojisoo20191941 <amy0480@naver.com>
2023-08-08 13:34:43 +0900

    [ feat ] 역할 리코일 풀리지 않도록 persist 추가

--- src/recoil/common/role.ts
+++ src/recoil/common/role.ts
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const role = atom({
   key: "role",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
