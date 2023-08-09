b0242024255f84982ba49bf59062b2c18fe62432
Seojisoo20191941 <amy0480@naver.com>
2023-08-09 20:45:11 +0900

    [ feat ] showmore 컴포넌트 추가

--- src/@pages/trackPostPage.tsx
+++ src/@pages/trackPostPage.tsx
@@ -2,6 +2,7 @@ import AudioInfo from "../@components/trackPost/audioInfo";
 import AudioTitle from "../@components/trackPost/audioTitle";
 import Download from "../@components/trackPost/download";
 import ProducerProfile from "../@components/trackPost/producerProfile";
+import ShowMore from "../@components/trackPost/showMore";
 
 export default function TrackPostPage() {
   return (
@@ -9,6 +10,7 @@ export default function TrackPostPage() {
       <AudioTitle />
       <ProducerProfile />
       <Download />
+      <ShowMore />
       <AudioInfo />
     </>
   );
