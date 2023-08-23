4ef398349c7093ca7b359c6576e018a94485364f
Seojisoo20191941 <amy0480@naver.com>
2023-08-23 17:06:31 +0900

    [ etc ] 불필요 코드 삭제

--- src/api/trackPost/deleteComment.ts
+++ src/api/trackPost/deleteComment.ts
@@ -1,13 +1,11 @@
+import { getCookie } from "../../utils/common/cookie";
 import { client } from "../common/client";
 
 export async function deleteComment(commentId: number) {
-  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWJsZU5hbWUiOiJ2b2NhbCIsInVzZXJJZCI6MSwiaWF0IjoxNjkyMTkzMzM3LCJleHAiOjE2OTczNzczMzd9.ORQNliZNoDmNF4S8KsmPnfkmN4QkUqLONKQQukX-za8`;
-
   const data = await client.delete(`${process.env.REACT_APP_BASE_URL}/comments/${commentId}`, {
     headers: {
       "Content-Type": "application/json",
-      // Authorization: `Bearer ${getCookie("accessToken")}`,
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${getCookie("accessToken")}`,
       commentId: commentId,
     },
   });
