810d97b36120e7fa968a35bccb1f28b7ca317bc9
Seojisoo20191941 <amy0480@naver.com>
2023-08-23 14:29:44 +0900

    [ etc ] 주석 처리

--- src/@components/trackPost/commentBox.tsx
+++ src/@components/trackPost/commentBox.tsx
@@ -121,12 +121,12 @@ const CommentContainer = styled.article<{ commentActive: boolean }>`
       linear-gradient(to right, ${({ theme }) => theme.colors.sub2}, ${({ theme }) => theme.colors.sub3});
   }
 
-  &[data-play="true"] {
-    background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
-      linear-gradient(
-        to right,
-        ${({ theme, commentActive }) => commentActive && theme.colors.sub2},
-        ${({ theme }) => theme.colors.sub3}
-      );
-  }
+  /* &[data-play="true"] { */
+  background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
+    linear-gradient(
+      to right,
+      ${({ theme, commentActive }) => commentActive && theme.colors.sub2},
+      ${({ theme }) => theme.colors.sub3}
+    );
+  /* } */
 `;
