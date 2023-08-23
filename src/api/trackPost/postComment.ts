486080a6487482eac2de1bca040d8151fed2da51
Seojisoo20191941 <amy0480@naver.com>
2023-08-23 17:15:12 +0900

    [ etc ] 불필요 코드 삭제

--- src/api/trackPost/postComment.ts
+++ src/api/trackPost/postComment.ts
@@ -1,13 +1,12 @@
 import { CommentDataType } from "../../type/trackPost/commentDataType";
+import { getCookie } from "../../utils/common/cookie";
 import { client } from "../common/client";
 
 export async function postComment(commentData: CommentDataType, trackId: number) {
-  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWJsZU5hbWUiOiJ2b2NhbCIsInVzZXJJZCI6MSwiaWF0IjoxNjkyMTkzMzM3LCJleHAiOjE2OTczNzczMzd9.ORQNliZNoDmNF4S8KsmPnfkmN4QkUqLONKQQukX-za8`;
   const data = await client.post(`${process.env.REACT_APP_BASE_URL}/comments/${trackId}`, commentData, {
     headers: {
       "Content-Type": "multipart/form-data",
-      // Authorization: `Bearer ${getCookie("accessToken")}`,
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${getCookie("accessToken")}`,
       trackId: trackId,
     },
   });
--- src/api/trackPost/updateComment.ts
+++ src/api/trackPost/updateComment.ts
@@ -1,14 +1,12 @@
 import { CommentDataType } from "../../type/trackPost/commentDataType";
+import { getCookie } from "../../utils/common/cookie";
 import { client } from "../common/client";
 
 export async function updateComment(commentData: CommentDataType, commentId: number) {
-  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWJsZU5hbWUiOiJ2b2NhbCIsInVzZXJJZCI6MSwiaWF0IjoxNjkyMTkzMzM3LCJleHAiOjE2OTczNzczMzd9.ORQNliZNoDmNF4S8KsmPnfkmN4QkUqLONKQQukX-za8`;
-
   const data = await client.patch(`${process.env.REACT_APP_BASE_URL}/comments/${commentId}`, commentData, {
     headers: {
       "Content-Type": "multipart/form-data",
-      // Authorization: `Bearer ${getCookie("accessToken")}`,
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${getCookie("accessToken")}`,
     },
   });
 
