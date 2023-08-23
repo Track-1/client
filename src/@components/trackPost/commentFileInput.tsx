1010b768b9a0d55744ec08a731247afbc67db931
Seojisoo20191941 <amy0480@naver.com>
2023-08-23 13:12:48 +0900

    [ etc ] 불필요 코드 삭제

--- src/@components/trackPost/commentFileInput.tsx
+++ src/@components/trackPost/commentFileInput.tsx
@@ -9,27 +9,6 @@ export default function CommentFileInput() {
   const [comment, setComment] = useRecoilState(commentWriteData);
   const { audioInit, uploadAudiofile } = useUploadAudioFile();
 
-  // const commentFile = useRef<HTMLInputElement | null>(null);
-
-  // function handleUploadFile(e: React.ChangeEvent<HTMLInputElement>) {
-  //   const currentFile = e.target.files && e.target.files[0];
-  //   currentFile && setComment({ ...comment, commentAudioFile: currentFile, commentAudioFileName: currentFile?.name });
-
-  //   if (e.target.files !== null) {
-  //     const file = e.target.value;
-  //     const audioFileName: string = getAudioFileName(file);
-  //     const audioFileType: string = getAudioFileType(file, audioFileName.length);
-  //     if (e.target.files?.length === 0) {
-  //       // alert("파일삽입이 취소되었습니다.")
-  //     } else {
-  //       if (!checkAudioFileType(audioFileType)) {
-  //         alert("Only wav, mp3 format audio can be uploaded.\nwav, mp3형식의 오디오만 업로드할 수 있습니다.");
-  //         setComment({ ...comment, commentAudioFile: null, commentAudioFileName: "file_upload.mp3" });
-  //       }
-  //     }
-  //   }
-  // }
-
   useEffect(() => {
     if (!audioInit?.audioFile || audioInit?.fileName === "") return;
     setComment({ ...comment, commentAudioFileName: audioInit?.fileName, commentAudioFile: audioInit?.audioFile });
