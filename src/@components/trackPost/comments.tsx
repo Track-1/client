870208e6b2d78d219571e7781799f4eac48531ca
Seojisoo20191941 <amy0480@naver.com>
2023-08-23 14:25:26 +0900

    [ feat ] 댓글 업로드 api 연결

--- src/@components/trackPost/comments.tsx
+++ src/@components/trackPost/comments.tsx
@@ -1,7 +1,13 @@
+import { useMutation, useQueryClient } from "react-query";
+import { useParams } from "react-router-dom";
+import { useRecoilState, useResetRecoilState } from "recoil";
 import styled from "styled-components";
+import { postComment } from "../../api/trackPost/postComment";
 import { AddCommentIc, CloseCommentsBtnIc, ClosedAddCommentIc } from "../../assets";
+import { QUERIES_KEY } from "../../core/common/queriesKey";
 import useGetComment from "../../hooks/trackPost/useGetComment";
 import useGetTrackInfo from "../../hooks/trackPost/useGetTrackInfo";
+import { commentWriteData } from "../../recoil/trackPost/commentWriteData";
 import { CommentType } from "../../type/trackPost/commentType";
 import CommentBox from "./commentBox";
 import CommentLayout from "./commentLayout";
@@ -10,12 +16,35 @@ import CommentWrite from "./commentWrite";
 export default function Comments() {
   const { trackComments } = useGetComment(1);
   const { trackClosed } = useGetTrackInfo();
+  const [comment, setComment] = useRecoilState(commentWriteData);
+  const resetComment = useResetRecoilState(commentWriteData);
+  const { id } = useParams();
+
+  const queryClient = useQueryClient();
+
+  const { mutate: uploadComment } = useMutation(() => postComment(comment, Number(id)), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERIES_KEY.GET_TRACK_COMMENT);
+      resetComment();
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  function handleUploadComment() {
+    if (comment?.commentAudioFile && comment?.commentContent?.length > 0) {
+      uploadComment();
+    }
+  }
 
   return (
     <CommentLayout>
       <CloseCommentsBtnIcon />
       <CommentWrite />
-      <AddCommentIconWrapper>{!trackClosed ? <AddCommentIcon /> : <ClosedAddCommentIcon />}</AddCommentIconWrapper>
+      <AddCommentIconWrapper>
+        {!trackClosed ? <AddCommentIcon onClick={handleUploadComment} /> : <ClosedAddCommentIcon />}
+      </AddCommentIconWrapper>
       {trackComments?.map((eachComment: CommentType) => (
         <CommentBox key={eachComment?.commentId} eachComment={eachComment} />
       ))}
--- src/api/trackPost/postComment.ts
+++ src/api/trackPost/postComment.ts
@@ -1,17 +1,16 @@
-import { UploadDataType } from "../../type/upload/useUploadDataType";
-import { getCookie } from "../../utils/common/cookie";
+import { CommentDataType } from "../../type/trackPost/commentDataType";
 import { client } from "../common/client";
 
-export async function postComment(formData: UploadDataType, beatId: any) {
-  try {
-    const data = await client.post(`${process.env.REACT_APP_BASE_URL}/tracks/comments/${beatId}`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${getCookie("accessToken")}`,
-        trackId: beatId,
-      },
-    });
-  } catch (e) {
-    console.log(e);
-  }
+export async function postComment(commentData: CommentDataType, trackId: number) {
+  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWJsZU5hbWUiOiJ2b2NhbCIsInVzZXJJZCI6MSwiaWF0IjoxNjkyMTkzMzM3LCJleHAiOjE2OTczNzczMzd9.ORQNliZNoDmNF4S8KsmPnfkmN4QkUqLONKQQukX-za8`;
+  const data = await client.post(`${process.env.REACT_APP_BASE_URL}/comments/${trackId}`, commentData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      // Authorization: `Bearer ${getCookie("accessToken")}`,
+      Authorization: `Bearer ${token}`,
+      trackId: trackId,
+    },
+  });
+
+  return data;
 }
