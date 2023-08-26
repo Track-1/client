fda9c4158be9316c5c599f3b8ff78e5dd5b87aa3
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-27 03:11:34 +0900

    [ feat ] query keys 상수화

--- src/core/common/queriesKey.ts
+++ src/core/common/queriesKey.ts
@@ -1,3 +1,8 @@
 export const QUERIES_KEY = {
   GET_TRACK_INFO: "getTrackInfo",
-};
+  LOGOUT: "logout",
+  ACCESS_TOKEN: "accessToken",
+  TOKEN_VERIFY: "tokenVerify",
+  TRACK_DETAIL: "trackDetail",
+  TRACK_DOWNLOAD: "trackDownload",
+} as const;
