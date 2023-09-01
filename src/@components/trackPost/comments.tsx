6ff18372f5e46af897344f5d8eb9a7865720673c
Seojisoo20191941 <amy0480@naver.com>
2023-09-01 16:58:09 +0900

    [ etc ] 무한스크롤 시도 중

--- src/@components/trackPost/comments.tsx
+++ src/@components/trackPost/comments.tsx
@@ -48,7 +48,7 @@ export default function Comments(props: CommentsProp) {
     }
     // }
   }
-  console.log(trackComments?.pages[0]);
+  console.log(trackComments?.pages[0]?.nextPage);
 
   return (
     <CommentLayout>
