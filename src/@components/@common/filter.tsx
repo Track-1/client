8b7c54f68ef3798531da1e251421284280e62fa5
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-05 19:04:59 +0900

    design: pageType에 따른 UI동적 변경

--- src/@components/@common/filter.tsx
+++ src/@components/@common/filter.tsx
@@ -1,7 +1,13 @@
-import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
 import { CategoryId } from "../../core/common/categories";
+import { PageType } from "../../type/common/pageType";
+import { getInvariantObjectKeys, invariantOf } from "../../utils/common/invarientType";
+import { updateQueryParams } from "../../utils/common/queryString";
 import { Select } from "./selectBox";
-const CategoryItem = styled.div`
+
+const CategoryItem = styled.div<{ pageType: PageType; isSelected?: boolean }>`
   ${({ theme }) => theme.fonts.id}
   display: flex;
   justify-content: space-between;
@@ -17,33 +23,88 @@ const CategoryItem = styled.div`
   border: 0.15rem solid transparent;
   border-radius: 32px;
 
-  background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
-    linear-gradient(
-      to right,
-      ${({ theme }) => theme.colors.sub3} 0%,
-      ${({ theme }) => theme.colors.sub3} 20%,
-      ${({ theme }) => theme.colors.sub1} 100%
-    );
-  background-origin: border-box;
-  background-clip: content-box, border-box;
+  color: ${({ theme }) => theme.colors.white};
 
-  color: ${({ theme }) => theme.colors.sub1};
+  ${({ pageType, isSelected }) =>
+    pageType === "tracks" &&
+    isSelected &&
+    css`
+      background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
+        linear-gradient(
+          to right,
+          ${({ theme }) => theme.colors.sub3} 0%,
+          ${({ theme }) => theme.colors.sub3} 20%,
+          ${({ theme }) => theme.colors.sub1} 100%
+        );
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+      color: ${({ theme }) => theme.colors.sub1};
+    `}
 
-  & p {
-    margin-right: 2rem;
-  }
+  ${({ pageType }) =>
+    pageType === "vocals" &&
+    css`
+      background-image: linear-gradient(${({ theme }) => theme.colors.sub3}, ${({ theme }) => theme.colors.sub3}),
+        linear-gradient(
+          to right,
+          ${({ theme }) => theme.colors.sub3} 0%,
+          ${({ theme }) => theme.colors.sub3} 20%,
+          ${({ theme }) => theme.colors.sub2} 100%
+        );
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+      color: ${({ theme }) => theme.colors.sub2};
+    `}
 `;
 
-export default function Filter() {
+const CategoryCancelButton = styled.button<{ pageType: PageType; isSelected?: boolean }>`
+  margin-right: 2rem;
+
+  ${({ pageType, isSelected }) =>
+    pageType === "tracks" &&
+    isSelected &&
+    css`
+      color: ${({ theme }) => theme.colors.sub1};
+    `}
+
+  ${({ pageType, isSelected }) =>
+    pageType === "vocals" &&
+    isSelected &&
+    css`
+      color: ${({ theme }) => theme.colors.sub2};
+    `}
+`;
+
+interface FilterProps {
+  pageType: PageType;
+}
+
+export default function Filter(props: FilterProps) {
+  const { pageType } = props;
+  const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState<Set<string>>(new Set());
+
+  function selectCategory(id: number | null) {
+    const tempSelectedCategory = new Set(selectedCategory);
+    tempSelectedCategory.add(String(id));
+    setSelectedCategory(tempSelectedCategory);
+  }
+
+  useEffect(() => {
+    navigate(updateQueryParams("categ", Array.from(selectedCategory)));
+  }, [selectedCategory]);
+
   return (
-    <Select defaultOpen>
+    <Select defaultOpen externalSelectState={selectCategory}>
       <Select.OptionGroup asChild={false}>
-        {Object.keys(CategoryId).map((category) => {
+        {getInvariantObjectKeys(invariantOf(CategoryId)).map((category) => {
           return (
-            <Select.Option key={CategoryId[category]} id={Number(CategoryId[category])} asChild>
-              <CategoryItem>
+            <Select.Option key={CategoryId[category]} id={Number(CategoryId[category])} isUnSelectable asChild>
+              <CategoryItem pageType={pageType}>
                 {category}
-                <p>x</p>
+                <Select.Indicator id={Number(CategoryId[category])} asChild>
+                  <CategoryCancelButton pageType={pageType}>x</CategoryCancelButton>
+                </Select.Indicator>
               </CategoryItem>
             </Select.Option>
           );
