d1651d2f5586d2a031c680d827b4ddb92d0519a4
myeongheonhong <ruddyh7076@gmail.com>
2023-09-18 13:42:14 +0900

    [ style ] 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -87,3 +87,6 @@ export { ReactComponent as DefaultToggleIc } from "./icon/defaultToggleIc.svg";
 export { ReactComponent as ProducerTypeToggleIc } from "./icon/producerTypeToggleIc.svg";
 export { ReactComponent as HomeLogoIc } from "./icon/homeLogoIc.svg";
 export { ReactComponent as SloganIc } from "./icon/sloganIc.svg";
+export { ReactComponent as RequestBlackTextIc } from "./icon/requestBlackTextIc.svg";
+export { ReactComponent as RequestWhiteTextIc } from "./icon/requestWhiteTextIc.svg";
+export { ReactComponent as ResendTextIc } from "./icon/resendTextIc.svg";
