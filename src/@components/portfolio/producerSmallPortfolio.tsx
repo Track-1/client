341f7e4ce4e4c7262d2f2f0642b478ea07bba604
Seojisoo20191941 <amy0480@naver.com>
2023-09-23 05:09:18 +0900

    [ feat ] 작은 프로듀서 프로필 박스 구현

---
+++ src/@components/portfolio/producerSmallPortfolio.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { PortfolioPauseIc, PortfolioPlayIc } from "../../assets";
+
+interface ProducerSmallPortfolioProps {
+  portfolioId: number;
+  portfolioImageFile: string;
+  portfolioAudioFile: string;
+}
+
+export default function ProducerSmallPortfolio(props: ProducerSmallPortfolioProps) {
+  const { portfolioId, portfolioImageFile, portfolioAudioFile } = props;
+  // 플레이어 붙이기
+  const isPlay = true;
+  const [hover, setHover] = useState(-1);
+
+  function handleHoverImage() {
+    setHover(portfolioId);
+  }
+
+  function handleNoneHover() {
+    setHover(-1);
+  }
+
+  return (
+    <BigImageContainer onMouseEnter={handleHoverImage} onMouseLeave={handleNoneHover}>
+      <BigImageWrapper className="image-wrapper">
+        <Image src={portfolioImageFile} alt="포트폴리오 이미지" className="image" />
+      </BigImageWrapper>
+      {!isPlay ? hover !== -1 && <PortfolioPauseIcon /> : hover !== -1 && <PortfolioPlayIcon />}
+    </BigImageContainer>
+  );
+}
+
+const PortfolioPauseIcon = styled(PortfolioPauseIc)`
+  width: 5.2rem;
+  height: 5.2rem;
+  position: absolute;
+
+  cursor: pointer;
+`;
+
+const PortfolioPlayIcon = styled(PortfolioPlayIc)`
+  width: 5.2rem;
+  height: 5.2rem;
+  position: absolute;
+  cursor: pointer;
+`;
+
+const BigImageContainer = styled.div`
+  display: flex;
+  width: 21.8rem;
+  height: 21.8rem;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    .image-wrapper {
+      background-color: rgba(13, 14, 17, 0.5);
+
+      box-shadow: 0 0 4rem ${({ theme }) => theme.colors.sub1};
+    }
+    .image {
+      filter: blur(3.5rem);
+    }
+  }
+`;
+
+const BigImageWrapper = styled.div`
+  display: inline-block;
+
+  overflow: hidden;
+  width: 21.8rem;
+  height: 21.8rem;
+  border-radius: 50%;
+
+  /* margin: 12rem 0 0 12rem; */
+  /* margin-bottom: 12rem; */
+
+  cursor: pointer;
+`;
+
+const Image = styled.img`
+  border-radius: 50%;
+
+  object-fit: cover;
+
+  width: 100%;
+  height: 100%;
+
+  cursor: pointer;
+`;
