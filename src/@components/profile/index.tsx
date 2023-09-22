eebbde6b423ad57172f5df2777f2e167b0791143
Seojisoo20191941 <amy0480@naver.com>
2023-09-23 03:17:11 +0900

    [ style ] 컴포넌트 안으로 넣기

--- src/@components/profile/index.tsx
+++ src/@components/profile/index.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { SleeperAccountIc } from "../../assets";
+import { ProfileEditBtnIc, SleeperAccountIc } from "../../assets";
 import { UserProfileType } from "../../type/profile";
+import BackButton from "../@common/backButton";
 import ProducerProfileImage from "./producerProfileImage";
 import ProfileCategory from "./profileCategory";
 import ProfileDescription from "./profileDescription";
@@ -9,14 +11,26 @@ import VocalProfileImage from "./vocalProfileImage";
 
 interface ProfileProps {
   userType: string | undefined;
+  userSelf: boolean | undefined;
   userProfile: UserProfileType | undefined;
 }
 
 export default function Profile(props: ProfileProps) {
-  const { userType, userProfile } = props;
+  const { userType, userSelf, userProfile } = props;
+  const navigate = useNavigate();
+
+  function handleMoveProfileEditPage() {
+    userType === "vocal"
+      ? navigate(`/profile-edit/vocal/${userProfile?.userId}`)
+      : navigate(`/profile-edit/producer/${userProfile?.userId}`);
+  }
 
   return (
     <ProfileWrapper>
+      <BackButtonWrapper>
+        <BackButton />
+        {userSelf && <ProfileEditBtnIcon onClick={handleMoveProfileEditPage} />}
+      </BackButtonWrapper>
       {userType === "vocal" && <VocalProfileImage />}
       {userType === "producer" && <ProducerProfileImage />}
       <Title>
@@ -32,6 +46,21 @@ export default function Profile(props: ProfileProps) {
     </ProfileWrapper>
   );
 }
+
+const BackButtonWrapper = styled.div`
+  margin: 6rem 0 6rem 0;
+  display: flex;
+  padding: 0 8rem;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const ProfileEditBtnIcon = styled(ProfileEditBtnIc)`
+  width: 16.6rem;
+  margin-left: 16.9rem;
+
+  cursor: pointer;
+`;
 
 const Title = styled.header`
   margin-top: 2.1rem;
--- src/@components/vocalProfile/index.tsx
+++ src/@components/vocalProfile/index.tsx
@@ -1,41 +1,14 @@
-import { useNavigate, useParams } from "react-router-dom";
-import styled from "styled-components";
-import { ProfileEditBtnIc } from "../../assets";
+import { useParams } from "react-router-dom";
 import useGetVocalProfile from "../../hooks/vocalProfile/useGetVocalProfile";
-import BackButton from "../@common/backButton";
 import Profile from "../profile";
 
 export default function VocalProfile() {
   const { vocalId } = useParams();
   const { vocalProfile } = useGetVocalProfile(Number(vocalId));
-  const navigate = useNavigate();
-
-  function handleMoveProfileEditPage() {
-    navigate(`/profile-edit/vocal/${vocalProfile?.userProfile.userId}`);
-  }
 
   return (
     <>
-      <BackButtonWrapper>
-        <BackButton />
-        {vocalProfile?.userSelf && <ProfileEditBtnIcon onClick={handleMoveProfileEditPage} />}
-      </BackButtonWrapper>
-      <Profile userType="vocal" userProfile={vocalProfile?.userProfile} />
+      <Profile userType="vocal" userSelf={vocalProfile?.userSelf} userProfile={vocalProfile?.userProfile} />
     </>
   );
 }
-
-const BackButtonWrapper = styled.div`
-  margin: 6rem 0 6rem 0;
-  display: flex;
-  padding: 0 8rem;
-  width: 100%;
-  justify-content: space-between;
-`;
-
-const ProfileEditBtnIcon = styled(ProfileEditBtnIc)`
-  width: 16.6rem;
-  margin-left: 16.9rem;
-
-  cursor: pointer;
-`;
