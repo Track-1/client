87b979cc19c747f500d91f7914e45128248bbfc2
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-25 21:03:39 +0900

    [ feat ] 무한스크롤 커스텀훅 생성

---
+++ src/hooks/common/useInfiniteScroll.ts
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export default function useInfiniteScroll(fetchNextPage: any, hasNextPage: any) {
+  const observerRef = useRef(null);
+
+  const handleObserver = useCallback(
+    (entries: any) => {
+      const [target] = entries;
+      if (target.isIntersecting) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage],
+  );
+
+  useEffect(() => {
+    const element = observerRef.current;
+    const option = { threshold: 0 };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+    element && observer.observe(element);
+    return () => {
+      element && observer.unobserve(element);
+    };
+  }, [fetchNextPage, hasNextPage, handleObserver]);
+
+  return { observerRef };
+}
