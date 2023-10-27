52586d1dfd6ef4182259252e36c8e5fa4de1adc1
myeongheonhong <ruddyh7076@gmail.com>
2023-10-27 21:57:47 +0900

    [ feat ] 이벤트 api 커스텀훅으로 구현

---
+++ src/hooks/queries/admin/event.ts
@@ -0,0 +1,54 @@
+import { useInfiniteQuery, useMutation, useQuery } from "react-query";
+import { getEventDetail, getEventList, postEvent } from "../../../api/admin/event";
+import { QUERIES_KEY } from "../../../core/common/queriesKey";
+import { EventListParamsType } from "../../../type/event";
+
+export function useGetEventList(params: EventListParamsType) {
+  const fetchEvents = async (pageParams: number) => {
+    const response = await getEventList({ ...params, page: pageParams });
+
+    return { response, nextPage: pageParams + 1 };
+  };
+
+  const { data, fetchNextPage, hasNextPage, ...restValues } = useInfiniteQuery(
+    [QUERIES_KEY.GET_EVENT_LIST, params.limit],
+    ({ pageParam = 1 }) => fetchEvents(pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.response.data[0].eventList.length === 0 ? undefined : lastPage.nextPage;
+      },
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  const eventListData = data?.pages.flatMap((data) => data.response.data[0].eventList.map((eventInfo) => eventInfo));
+
+  return {
+    eventListData,
+    fetchNextPage,
+    hasNextPage,
+    ...restValues,
+  };
+}
+
+export function useGetEventDetail(eventId: number) {
+  const { data: eventDetailData } = useQuery(["getEventDetail"], () => getEventDetail(eventId), {
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
+  return { eventDetailData };
+}
+
+export function useUploadEvent() {
+  const { mutate, ...restValues } = useMutation({
+    mutationFn: (formData: FormData) => postEvent(formData),
+    onSuccess: () => {},
+    onError: () => {},
+  });
+  return {
+    uploadEvent: mutate,
+    ...restValues,
+  };
+}
