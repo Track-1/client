9dd9d289c178f1016385c50a863042def7bce7c6
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-11-05 04:09:10 +0900

    [ design ] 공통 스타일 컴포넌트 파일 생성

---
+++ src/@components/@common/styledComponents.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.section`
+  position: absolute;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 77.9rem;
+
+  backdrop-filter: blur(1rem);
+  border: 0.3rem solid transparent;
+  border-radius: 5rem;
+  background-image: linear-gradient(rgba(20, 21, 23, 0.6), rgba(20, 21, 23, 0.6)),
+    linear-gradient(to top, transparent, #3e4045);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+`;
+
+export const InputContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  width: 55.9rem;
+`;
+
+export const InputTitle = styled.h2`
+  margin-bottom: 3rem;
+
+  color: ${({ theme }) => theme.colors.gray2};
+  ${({ theme }) => theme.fonts.body1};
+`;
