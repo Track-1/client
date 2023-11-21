802af087ce4867f2d9d92f67f1a4d4416ff96da0
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-11-21 21:37:07 +0900

    [ feat ] form타입 추가

--- src/type/common/upload.ts
+++ src/type/common/upload.ts
@@ -1,3 +1,13 @@
-import { UPLOAD_TYPE } from "../../core/common/uploadType";
+import { PRODUCER_UPLOAD_TYPE } from "../../core/common/uploadType";
 
-export type UploadType = typeof UPLOAD_TYPE[keyof typeof UPLOAD_TYPE];
+export type ProducerUploadType = typeof PRODUCER_UPLOAD_TYPE[keyof typeof PRODUCER_UPLOAD_TYPE];
+
+export type UploadInputType = {
+  image: string;
+  title: string;
+  audioFile: FileList;
+  hashtag: string[];
+  description: string;
+};
+
+export type UploadPageType = "track" | "vocal" | "producer";
