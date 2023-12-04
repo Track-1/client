3a0c7dcd2ffb40ee98156a172b4686527abc4150
myeongheonhong <ruddyh7076@gmail.com>
2023-12-04 22:17:15 +0900

    [ chore ] prettierrc 설정 및 기타 환경설정

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,13 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ["prettier", "plugin:react/jsx-runtime"],
-  plugins: ["@typescript-eslint/eslint-plugin", "eslint-plugin-react", "@typescript-eslint", "import"],
-  parser: "@typescript-eslint/parser",
+  root: true,
+  files: ['*.ts', '*.tsx'],
+  extends: ['prettier', 'plugin:react/jsx-runtime'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-react', '@typescript-eslint', 'import'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["../tsconfig/base.json"],
+    project: ['/packages/tsconfig/base.json'],
+    createDefaultProgram: true,
   },
 };
--- packages/eslint-config-custom/prettier-preset.js
+++ packages/eslint-config-custom/prettier-preset.js
@@ -1,12 +1,14 @@
 module.exports = {
-  bracketSpacing: true,
-  bracketSameLine: true,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  trailingComma: "es5",
-  semi: true,
-  printWidth: 110,
-  arrowParens: "always",
-  endOfLine: "auto",
-  importOrderSeparation: true,
+  bracketSpacing: true, // 객체 괄호에 공백 삽입
+  bracketSameLine: true, //마지막 `>`를 다음 줄로 내릴지 여부
+  singleQuote: true, // 쌍따옴표 대신 홑따옴표 사용
+  jsxSingleQuote: false, // JSX에 singe 쿼테이션 사용 여부
+  trailingComma: 'es5', // 가능하면 후행 쉼표 사용
+  semi: true, // 모든 구문 끝에 세미콜론 출력
+  printWidth: 120, // 줄 바꿈할 길이
+  arrowParens: 'always', //화살표 함수에서 매개변수는 모두 괄호로 처리
+  endOfLine: 'auto', // OS에 따른 코드라인 끝 처리 방식 사용
+  importOrderSeparation: false, //import 각 범주마다 공백을 줄지 말지
+  useTabs: false, // 탭 대신 공백으로 들여쓰기
+  tabWidth: 2, // 들여쓰기 공백 수
 };
