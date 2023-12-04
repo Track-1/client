e73c726c1e4f4cd3e5352fba2d5787766810c0c8
myeongheonhong <ruddyh7076@gmail.com>
2023-12-04 22:20:55 +0900

    [ chore ] 기타 코드 수정 및 푸터 컴포넌트 추가

--- apps/mobile/src/pages/mainPage.tsx
+++ apps/mobile/src/pages/mainPage.tsx
@@ -1,11 +1,11 @@
-import Header from "../components/header";
-import MainContainer from "../components/main";
+import Header from '../components/header';
+import Layout from '../components/layout';
+import MainContainer from '../components/main';
 
 export default function MainPage() {
   return (
-    <>
-      <Header />
+    <Layout layoutKey='header'>
       <MainContainer />
-    </>
+    </Layout>
   );
 }
