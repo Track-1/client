4340334aa39e961642743f0f1f3a5ba21ef0db65
myeongheonhong <ruddyh7076@gmail.com>
2023-12-04 22:18:57 +0900

    [ chore ] 상위 파일 감싸주는 컴포넌트 재설정

--- apps/mobile/src/index.tsx
+++ apps/mobile/src/index.tsx
@@ -1,21 +1,8 @@
-import { CookiesProvider } from "react-cookie";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-    },
-  },
-});
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-root.render(
-  <CookiesProvider>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </CookiesProvider>,
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+
+root.render(<App />);
