4769df64ee3a95ab536a7fc52c31d56916338a91
myeongheonhong <ruddyh7076@gmail.com>
2023-12-09 22:36:32 +0900

    [ design ] 이미지 추가

--- apps/mobile/src/assets/index.ts
+++ apps/mobile/src/assets/index.ts
@@ -1,2 +1,5 @@
-export { ReactComponent as HomeLogoIc } from "./icon/homeLogoIc.svg";
-export { ReactComponent as HamburgerMenuIc } from "./icon/hamburgerMenuIc.svg";
+export { ReactComponent as HomeLogoIc } from './icon/homeLogoIc.svg';
+export { ReactComponent as HamburgerMenuIc } from './icon/hamburgerMenuIc.svg';
+export { ReactComponent as MoreBtnIc } from './icon/moreBtnIc.svg';
+export { ReactComponent as PlayIc } from './icon/playIc.svg';
+export { ReactComponent as PauseIc } from './icon/pauseIc.svg';
