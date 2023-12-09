db246a0225c3bc028add5dfd2b3837145a9a19ff
myeongheonhong <ruddyh7076@gmail.com>
2023-12-09 22:03:47 +0900

    [ feat ] 메인에서 사용하는 공통컴포넌트 구현

---
+++ apps/mobile/src/components/common/Form/playTrackForm.tsx
@@ -0,0 +1,49 @@
+import styled, { CSSProperties, css } from 'styled-components';
+import { PauseIc, PlayIc } from '../../../assets';
+
+interface PlayTrackForm {
+  iconProperties: CSSProperties;
+  shapeProperties: CSSProperties;
+  isPlaying: boolean;
+}
+
+export default function PlayTrackForm(props: any) {
+  const { iconProperties, shapeProperties, isPlaying } = props;
+
+  return (
+    <Container>
+      <ImageWrapper isPlaying={isPlaying} style={shapeProperties}>
+        <IconWrapper style={iconProperties}>{isPlaying ? <PauseIc /> : <PlayIc />}</IconWrapper>
+      </ImageWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: gray;
+  overflow: hidden;
+`;
+
+const ImageWrapper = styled.div<{ isPlaying: boolean }>`
+  width: 100%;
+  height: 100%;
+
+  background-color: orange;
+
+  object-fit: cover;
+
+  ${(props) =>
+    props.isPlaying
+      ? css`
+          background: rgba(13, 14, 17, 0.5);
+        `
+      : css`
+          background: transparent;
+        `}
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ apps/mobile/src/components/event/eventContainer.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+interface EventContainerProps {
+  eventImage: string;
+  eventTitle: string;
+}
+
+export default function EventContainer(props: EventContainerProps) {
+  const { eventImage, eventTitle } = props;
+  return (
+    <Container eventImage={eventImage}>
+      <EventTitle>{eventTitle}</EventTitle>
+    </Container>
+  );
+}
+
+const Container = styled.div<{ eventImage: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 22.5rem;
+
+  overflow: hidden;
+  background-image: url(${(props) => props.eventImage});
+  background-position: center center;
+`;
+
+const EventTitle = styled.h2`
+  ${({ theme }) => theme.fonts.Alex_30_R};
+  color: ${({ theme }) => theme.colors.white};
+`;
