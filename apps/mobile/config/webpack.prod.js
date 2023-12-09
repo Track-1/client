46fbcec33bdc35e7182013550190a1425609b48d
myeongheonhong <ruddyh7076@gmail.com>
2023-12-09 23:19:22 +0900

    [ chore ] 웹팩 배포버전 엔트리 설정 변경

--- apps/mobile/config/webpack.prod.js
+++ apps/mobile/config/webpack.prod.js
@@ -1,25 +1,26 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
-const path = require("path");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const path = require('path');
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "cheap-module-source-map",
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "./",
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: './',
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -38,7 +39,7 @@ module.exports = merge(common, {
       new CssMinimizerPlugin(),
     ],
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   performance: {
