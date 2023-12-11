2398b2ab037445bbe7f91ea1cdaac8ddd543a2ae
myeongheonhong <ruddyh7076@gmail.com>
2023-12-11 15:09:46 +0900

    [ feat ] 구분선 컴포넌트 구현

---
+++ apps/mobile/src/components/common/DivisionLine/index.tsx
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components';
+import { ColorsTypes } from '../../../style/theme';
+
+export const StyledLined = styled.hr<{ color?: keyof ColorsTypes }>`
+  width: 100%;
+  height: 1px;
+
+  border: none;
+
+  background-color: ${(props) =>
+    props.color ? ({ theme }) => theme.colors[props.color as keyof ColorsTypes] : ({ theme }) => theme.colors.gray5};
+`;
