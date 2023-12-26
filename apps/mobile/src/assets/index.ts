a59c8d8f03a636c33d6da8840ca2528f59b64304
myeongheonhong <ruddyh7076@gmail.com>
2023-12-26 20:57:54 +0900

    [ design ] 이미지 파일 추가

--- apps/mobile/src/assets/index.ts
+++ apps/mobile/src/assets/index.ts
@@ -3,3 +3,6 @@ export { ReactComponent as HamburgerMenuIc } from './icon/hamburgerMenuIc.svg';
 export { ReactComponent as MoreBtnIc } from './icon/moreBtnIc.svg';
 export { ReactComponent as PlayIc } from './icon/playIc.svg';
 export { ReactComponent as PauseIc } from './icon/pauseIc.svg';
+export { ReactComponent as Track1LogoIc } from './icon/track1LogoIc.svg';
+export { ReactComponent as PasswordActiveIc } from './icon/passwordActiveIc.svg';
+export { ReactComponent as PasswordUnActiveIc } from './icon/passwordUnActiveIc.svg';
