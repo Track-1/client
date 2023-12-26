03ded4070153c9d3715155b124bee417263bdfae
myeongheonhong <ruddyh7076@gmail.com>
2023-12-26 21:16:02 +0900

    [ chore ] 프리티어 설정변경으로 인한 커밋

--- apps/mobile/src/Router.tsx
+++ apps/mobile/src/Router.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import MainPage from "./pages/mainPage";
-import ProducerProfilePage from "./pages/producerProfilePage";
-import SignupProfilePage from "./pages/signupProfilePage";
-import SignupStepPage from "./pages/signupStepPage";
-import SignupSuccessPage from "./pages/signupSuccessPage";
-import TrackPostPage from "./pages/trackPostPage";
-import ErrorPage from "./pages/errorPage";
-import LoginPage from "./pages/loginPage";
-import TrackSearchPage from "./pages/trackSearchPage";
-import VocalProfilePage from "./pages/vocalProfilePage";
-import VocalSearchPage from "./pages/vocalSearchPage";
-import PrivateRoute from "./utils/common/privateRouter";
-import EventPage from "./pages/eventPage";
-import AboutPage from "./pages/aboutPage";
+import MainPage from './pages/mainPage';
+import ProducerProfilePage from './pages/producerProfilePage';
+import SignupProfilePage from './pages/signupProfilePage';
+import SignupStepPage from './pages/signupStepPage';
+import SignupSuccessPage from './pages/signupSuccessPage';
+import TrackPostPage from './pages/trackPostPage';
+import ErrorPage from './pages/errorPage';
+import LoginPage from './pages/loginPage';
+import TrackSearchPage from './pages/trackSearchPage';
+import VocalProfilePage from './pages/vocalProfilePage';
+import VocalSearchPage from './pages/vocalSearchPage';
+import PrivateRoute from './utils/common/privateRouter';
+import EventPage from './pages/eventPage';
+import AboutPage from './pages/aboutPage';
 
 export default function Router() {
   return (
@@ -29,15 +29,15 @@ export default function Router() {
         <Route path="/vocal-search" element={<VocalSearchPage />} />
         <Route path="/track-post/:id" element={<TrackPostPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<ErrorPage />} />
+        {/* <Route path="*" element={<ErrorPage />} /> */}
 
         {/* 반드시 인증 필요 */}
-        <Route element={<PrivateRoute authentication={true} />}>
-          <Route path="/signup/profile" element={<SignupProfilePage />} />
-          <Route path="/signup/success" element={<SignupSuccessPage />} />
-          <Route path="/vocal-profile/:vocalId" element={<VocalProfilePage />} />
-          <Route path="/producer-profile/:producerId" element={<ProducerProfilePage />} />
-        </Route>
+        {/* <Route element={<PrivateRoute authentication={true} />}> */}
+        <Route path="/signup/profile" element={<SignupProfilePage />} />
+        <Route path="/signup/success" element={<SignupSuccessPage />} />
+        <Route path="/vocal-profile/:vocalId" element={<VocalProfilePage />} />
+        <Route path="/producer-profile/:producerId" element={<ProducerProfilePage />} />
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
