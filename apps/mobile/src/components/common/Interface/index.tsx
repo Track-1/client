965253324cc622508bf8a14188e2af915872e54f
myeongheonhong <ruddyh7076@gmail.com>
2023-12-27 16:37:14 +0900

    [ feat ] 아이콘 버튼사용을 위한 컴포넌트 구현

--- apps/mobile/src/components/common/Interface/index.tsx
+++ apps/mobile/src/components/common/Interface/index.tsx
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
 export const EmptyBox = styled.div``;
+
+export const IconButtonWrapper = styled.button<{ width: number; height: number }>`
+  width: ${(props) => props.width}rem;
+  height: ${(props) => props.height}rem;
+`;
