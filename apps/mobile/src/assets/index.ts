ba0e8bc28842479c8313097ca0ac8f63802f7e7b
myeongheonhong <ruddyh7076@gmail.com>
2023-12-29 11:26:25 +0900

    [ design ] 이미지 파일 추가

--- apps/mobile/src/assets/index.ts
+++ apps/mobile/src/assets/index.ts
@@ -6,3 +6,6 @@ export { ReactComponent as PauseIc } from './icon/pauseIc.svg';
 export { ReactComponent as Track1LogoIc } from './icon/track1LogoIc.svg';
 export { ReactComponent as PasswordVisableIc } from './icon/passwordVisableIc.svg';
 export { ReactComponent as PasswordUnVisableIc } from './icon/passwordUnVisableIc.svg';
+export { ReactComponent as CloseIc } from './icon/closeIc.svg';
+export { ReactComponent as DefaultUserIc } from './icon/defaultUserIc.svg';
+export { ReactComponent as RightArrowIc } from './icon/rightArrowIc.svg';
