2ea269469dc3bbed17a29c3e335e1a7fcea5fa67
myeongheonhong <ruddyh7076@gmail.com>
2023-12-31 01:39:44 +0900

    [ fix ] connect-history-api-fallback 오류로 인한 웹팩 설정 변경

--- apps/mobile/config/webpack.prod.js
+++ apps/mobile/config/webpack.prod.js
@@ -12,6 +12,7 @@ module.exports = merge(common, {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
   },
 
   optimization: {
