f7d5213ab5f55d02ae47f4cb85a3b174c20656ac
myeongheonhong <ruddyh7076@gmail.com>
2024-01-18 16:40:23 +0900

    [ feat ] 로딩컴포넌트 구현

--- apps/mobile/src/components/common/Loading/index.tsx
+++ apps/mobile/src/components/common/Loading/index.tsx
@@ -1,7 +1,33 @@
+import styled from 'styled-components';
+import { Z_INDEX } from '../../../core/common/zIndex';
+import mobile_loading from '../../../assets/image/mobile_loading.gif';
+
 export default function Loading() {
   return (
-    <>
-      <div>Loading</div>
-    </>
+    <Background>
+      <LoadingImage src={mobile_loading} />
+    </Background>
   );
 }
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  z-index: ${Z_INDEX.LOADING};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100svh;
+
+  background: rgba(0, 0, 0, 0.7);
+  backdrop-filter: blur(17.5px);
+`;
+
+const LoadingImage = styled.img`
+  width: 100%;
+`;
