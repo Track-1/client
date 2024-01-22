1d79054e8677d79040f477ddcf44ef5e533c4571
myeongheonhong <ruddyh7076@gmail.com>
2024-01-22 21:09:30 +0900

    [ refactor ] 컴포넌트, 상위 컴포넌트로 이동

--- apps/mobile/src/components/signUp/SignupStep/index.tsx
+++ apps/mobile/src/components/signUp/SignupStep/index.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
-import { SIGNUP_STEP } from '../../../core/signUp/stepRenderer';
 import ProgressBar from '../../common/ProgressBar';
 import Footer from '../common/Footer';
 import Header from '../../header';
-import { Layout, SignupHeader } from './Layout';
 import NicknameConvention from './NicknameConvention';
 import StepButtons from './StepButtons';
 import Title from './Title';
-import StepMain from './StepMain';
+import { useStep } from '../../../hooks/common/useStep';
+import Role from './Role';
+import EmailPassword from './EmailPassword';
+import styled from 'styled-components';
+import { StyledLayout } from '../../layout';
+// import Layout from '../../layout';
 
 export default function SingupStep() {
-  const [step, setStep] = useState(SIGNUP_STEP.ROLE);
+  const { step, moveToNextStep, moveToPrevStep } = useStep();
 
   return (
     <>
@@ -20,10 +22,30 @@ export default function SingupStep() {
           <ProgressBar progress={(step / 3) * 100} />
           <Title step={step} />
         </SignupHeader>
-        <StepMain step={step} />
-        <StepButtons step={step} setStep={setStep} />
+        {
+          {
+            1: <Role moveToNextStep={moveToNextStep} />,
+            2: <EmailPassword />,
+            3: <NicknameConvention />,
+          }[step]
+        }
+        <StepButtons step={step} moveToNextStep={moveToNextStep} moveToPrevStep={moveToPrevStep} />
         <Footer />
       </Layout>
     </>
   );
 }
+
+const Layout = styled(StyledLayout)`
+  justify-content: space-between;
+`;
+
+const SignupHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+
+  width: 100%;
+
+  margin: 4rem 0rem 6rem;
+`;
