968f201e8e1e8fc1bbf17ac91e92477679b7eaf7
myeongheonhong <ruddyh7076@gmail.com>
2024-01-24 10:44:19 +0900

    [ chore ] 불필요 코드 삭제 및 스타일 변경

--- apps/mobile/src/pages/trackSearchPage.tsx
+++ apps/mobile/src/pages/trackSearchPage.tsx
@@ -1,10 +1,7 @@
-import TrackFilterModal from '../components/common/Modal/Filter';
-import Player from '../components/common/Player/player';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Layout from '../components/layout';
 import TrackSearchContainer from '../components/trackSearch';
-import { PlayerProvider } from '../context/playerContext';
 
 export default function TrackSearchPage() {
   return (
