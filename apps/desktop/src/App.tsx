c69247de6e74889230052e1370abd6e88068b3cf
myeongheonhong <ruddyh7076@gmail.com>
2024-02-08 19:39:10 +0900

    [ feat ] 모바일 리다이렉트 코드 추가

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,15 +1,14 @@
-import { BrowserView, MobileView } from "react-device-detect";
-import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
-import MobileLandingPage from "./@pages/mobileLandingPage";
-import Router from "./Router";
-import { GlobalStyle } from "./style/globalStyle";
-import { theme } from "./style/theme";
-import { Suspense } from "react";
-import Loading from "./@components/@common/loading";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { CookiesProvider } from "react-cookie";
-
+import { BrowserView, MobileView } from 'react-device-detect';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import MobileLandingPage from './@pages/mobileLandingPage';
+import Router from './Router';
+import { GlobalStyle } from './style/globalStyle';
+import { theme } from './style/theme';
+import { Suspense, useEffect } from 'react';
+import Loading from './@components/@common/loading';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
   const queryClient = new QueryClient({
@@ -20,6 +19,28 @@ function App() {
       },
     },
   });
+
+  const isMobile = /Mobi/i.test(window.navigator.userAgent);
+
+  useEffect(() => {
+    const $existingMeta = document.querySelector('meta[name="viewport"]');
+
+    if (isMobile) {
+      window.location.href = 'https://m.track1.site';
+
+      const $meta = $existingMeta ?? document.createElement('meta');
+
+      $meta.setAttribute('name', 'viewport');
+      $meta.setAttribute(
+        'content',
+        'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'
+      );
+
+      if (!$existingMeta) {
+        document.head.appendChild($meta);
+      }
+    }
+  }, []);
 
   return (
     <CookiesProvider>
@@ -32,9 +53,6 @@ function App() {
                 <Router />
               </BrowserView>
             </Suspense>
-            <MobileView>
-              <MobileLandingPage />
-            </MobileView>
           </ThemeProvider>
         </RecoilRoot>
       </QueryClientProvider>
