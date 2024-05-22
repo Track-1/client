ad8e0156bdb3369c6e9e04fe3e43f841fe82821c
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2024-05-22 17:02:45 +0900

    fix: baseurl 제거

--- apps/desktop/src/api/common/client.ts
+++ apps/desktop/src/api/common/client.ts
@@ -3,7 +3,6 @@ import { getCookie, removeCookie, setCookie } from '../../utils/common/cookie';
 
 //서버통신 함수
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {},
   withCredentials: true,
 });
--- apps/desktop/src/index.tsx
+++ apps/desktop/src/index.tsx
@@ -3,13 +3,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-// root.render(<App />);
-
 async function enableMocking() {
   const { worker } = await import('../src/api/mocks/browser');
 
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
   return worker.start();
 }
 
